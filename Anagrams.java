581b4d692cac8b35526489111c1a90152797ee50
usunyu <usunyu@gmail.com>
2013-10-15 22:46:04 -0700

    Anagrams

---
+++ Anagrams.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+class StrInMap {
+    String str;
+    boolean used;
+
+    public StrInMap(String str) {
+        this.str = str;
+    }
+}
+
+class Solution {
+    public int hash(String str) {
+        int hash = 0;
+        int mul = 1;
+        int add = 0;
+        for(int i = 0; i < str.length(); i++) {
+            mul *= str.charAt(i);
+            add += str.charAt(i);
+        }
+        hash = mul + add + str.length();
+        return hash;
+    }
+    
+    // time complexity : O(N*M)
+    // space complexity : O(N)
+    public ArrayList<String> anagrams(String[] strs) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        HashMap<Integer, StrInMap> map = new HashMap<Integer, StrInMap>();
+        ArrayList<String> result = new ArrayList<String>();
+        for(int i = 0; i < strs.length; i++) {
+            int hash = hash(strs[i]);
+            if(!map.containsKey(hash)) {
+                StrInMap sim = new StrInMap(strs[i]);
+                map.put(hash, sim);
+            }
+            else {
+                StrInMap sim = map.get(hash);
+                if(!sim.used) {
+                    result.add(sim.str);
+                    sim.used = true;
+                }
+                result.add(strs[i]);
+            }
+        }
+        return result;
+    }
+}
+
+class Main {
+    public static void print(ArrayList<String> list) {
+        for(String str : list) {
+            System.out.print(str + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String[] strs = {"abc", "loy", "cba", "tt", "dd", "yol", "yol"};
+        ArrayList<String> result = solution.anagrams(strs);
+        print(result);
+    }
+}

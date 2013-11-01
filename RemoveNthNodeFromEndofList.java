050265d8c303b0a8595a8b9c528dab927c659351
usunyu <usunyu@gmail.com>
2013-10-31 19:39:08 -0700

    Remove Nth Node From End of List

---
+++ RemoveNthNodeFromEndofList.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+class Solution {
+    // time complexity : O(N)
+    // space complexity : O(N)
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        ArrayList<ListNode> buffer = new ArrayList<ListNode>();
+        // buffer the list
+        ListNode current = head;
+        int size = 0;
+        while(current != null) {
+            buffer.add(current);
+            current = current.next;
+            size++;
+        }
+        int index = size - n - 1;
+        if(index < 0) { // remove head
+            head = head.next;
+        }
+        else {  // remove middle
+            current = buffer.get(index);
+            current.next = current.next.next;
+        }
+        return head;
+    }
+}
+
+class Main {
+    public static ListNode constructList() {
+        ListNode head = null, prev = null;
+        for(int i = 0; i < 5; i++) {
+            ListNode node = new ListNode(i + 1);
+            if(i == 0) {
+                head = node;
+            }
+            else {
+                prev.next = node;
+            }
+            prev = node;
+        }
+        return head;
+    }
+
+    public static void print(ListNode head) {
+        ListNode current = head;
+        while(current != null) {
+            System.out.print(current.val);
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode head = constructList();
+        head = solution.removeNthFromEnd(head, 2);
+        print(head);
+    }
+}

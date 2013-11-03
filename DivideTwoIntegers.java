a381c75636a585de7d142e26b24ff95669af20b5
usunyu <usunyu@gmail.com>
2013-11-03 13:48:07 -0800

    Divide Two Integers using bit manipulation

--- DivideTwoIntegers.java
+++ DivideTwoIntegers.java
@@ -12,22 +12,46 @@ class Solution {
             sum += sum;
             exp += exp;
             if(sum > Integer.MAX_VALUE || sum > rest) {
-        	    sum = absDivisor;
-        	    exp = 1;
+                sum = absDivisor;
+                exp = 1;
             }
         }
         if((dividend < 0 && divisor < 0) || (dividend > 0 && divisor > 0)) {
-        	return result;
+            return result;
         }
         else {
-        	return 0 - result;
+            return 0 - result;
+        }
+    }
+
+    // using bit manipulation
+    public int divide2(int dividend, int divisor) {
+        int result = 0;
+        long absDividend = Math.abs((long)dividend), absDivisor = Math.abs((long)divisor);
+        long rest = absDividend, sum =  absDivisor;
+        int exp = 0;
+        while(rest >= absDivisor) {
+            result += (1 << exp);
+            rest -= sum;
+            sum = sum << 1;
+            exp++;
+            if(sum > Integer.MAX_VALUE || sum > rest) {
+                sum = absDivisor;
+                exp = 0;
+            }
+        }
+        if((dividend < 0 && divisor < 0) || (dividend > 0 && divisor > 0)) {
+            return result;
+        }
+        else {
+            return 0 - result;
         }
     }
 }
 
 class Main {
-	public static void main(String[] args) {
-		Solution solution = new Solution();
-		System.out.println(solution.divide(-1010369383, -2147483648));
-	}
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.divide2(92, 3));
+    }
 }

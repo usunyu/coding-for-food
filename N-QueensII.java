5549e9cd2e24b6a5e25373f37d7a16955270ccd1
usunyu <usunyu@gmail.com>
2013-11-09 15:39:04 -0800

    N-Queens II

---
+++ N-QueensII.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean checkValid(int[][] board, int row, int col) {
+        // check column
+        for(int i = 0; i < row; i++) {
+            if(board[i][col] == 1) return false;
+        }
+        // check hypotenuse
+        for(int i = row - 1; i >= 0; i--) {
+            int dis = row - i;
+            if(col + dis < board.length && board[i][col + dis] == 1) return false;
+            if(col - dis >= 0 && board[i][col - dis] == 1) return false;
+        }
+        return true;
+    }
+    
+    public int totalNQueens(int n, int level, int[][] board) {
+        if(level == n) return 1;
+        int num = 0;
+        for(int i = 0; i < n; i++) {
+            if(checkValid(board, level, i)) {
+                board[level][i] = 1;
+                num += totalNQueens(n, level + 1, board);
+                board[level][i] = 0;    // recover
+            }
+        }
+        return num;
+    }
+    
+    public int totalNQueens(int n) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        return totalNQueens(n, 0, new int[n][n]);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int num = solution.totalNQueens(4);
+        System.out.println(num);
+    }
+}

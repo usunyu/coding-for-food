053425ce45b41554802d64c714db5c54d9383daa
usunyu <usunyu@gmail.com>
2013-11-20 17:58:23 -0800

    Binary Tree Inorder Traversal

---
+++ BinaryTreeInorderTraversal.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+class Solution {
+    public void inorderTraversal(TreeNode root, ArrayList<Integer> result) {
+        if(root == null) return;
+        inorderTraversal(root.left, result);
+        result.add(root.val);
+        inorderTraversal(root.right, result);
+    }
+
+    // recursion solution
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        inorderTraversal(root, result);
+        return result;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode node1 = new TreeNode(2);
+        root.right = node1;
+        TreeNode node2 = new TreeNode(3);
+        node1.left = node2;
+        Solution solution = new Solution();
+        ArrayList<Integer> result = solution.inorderTraversal(root);
+        for(int i : result) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
+

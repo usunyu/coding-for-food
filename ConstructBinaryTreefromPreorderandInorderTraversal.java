432663a10322383522357cf8b7752ef63ba4d8f7
usunyu <usunyu@gmail.com>
2013-11-22 23:46:08 -0800

    Construct Binary Tree from Preorder and Inorder Traversal using HashMap

--- ConstructBinaryTreefromPreorderandInorderTraversal.java
+++ ConstructBinaryTreefromPreorderandInorderTraversal.java
@@ -8,6 +8,35 @@ class TreeNode {
 }
 
 class Solution {
+    // from BinaryTreeLevelOrderTraversal.java for testing
+    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        ArrayList<ArrayList<Integer>> levels = new ArrayList<ArrayList<Integer>>();
+        if(root == null) {
+            return levels;
+        }
+        LinkedList<TreeNode> nodeQueue = new LinkedList<TreeNode>();
+        nodeQueue.add(root);
+        while(!nodeQueue.isEmpty()) {
+            LinkedList<TreeNode> tempQueue = new LinkedList<TreeNode>();
+            ArrayList<Integer> level = new ArrayList<Integer>();
+            while(!nodeQueue.isEmpty()) {
+                TreeNode node = nodeQueue.removeFirst();
+                if(node.left != null) {
+                    tempQueue.add(node.left);
+                }
+                if(node.right != null) {
+                    tempQueue.add(node.right);
+                }
+                level.add(node.val);
+            }
+            // finish one level
+            levels.add(level);
+            nodeQueue = tempQueue;
+        }
+        return levels;
+    }
+
     public TreeNode buildTree(int[] preorder, int pstart, int pend, int[] inorder, int istart, int iend) {
         if(pstart <= pend && istart <= iend) {
             TreeNode local = new TreeNode(preorder[pstart]);
@@ -45,33 +74,33 @@ class Solution {
         return buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
     }
 
-    // from BinaryTreeLevelOrderTraversal.java for testing
-    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
-        ArrayList<ArrayList<Integer>> levels = new ArrayList<ArrayList<Integer>>();
-        if(root == null) {
-            return levels;
+    public TreeNode buildTree2(int[] preorder, int pstart, int pend, HashMap<Integer, Integer> inorderMap, int istart, int iend) {
+        if(pstart <= pend && istart <= iend) {
+            TreeNode local = new TreeNode(preorder[pstart]);
+            int i, j;
+            i = inorderMap.get(local.val);
+            j = (i - istart) + pstart + 1;
+            // build left
+            local.left = buildTree2(preorder, pstart + 1, j - 1, inorderMap, istart, i - 1);
+            // build right
+            local.right = buildTree2(preorder, j, pend, inorderMap, i + 1, iend);
+            return local;
         }
-        LinkedList<TreeNode> nodeQueue = new LinkedList<TreeNode>();
-        nodeQueue.add(root);
-        while(!nodeQueue.isEmpty()) {
-            LinkedList<TreeNode> tempQueue = new LinkedList<TreeNode>();
-            ArrayList<Integer> level = new ArrayList<Integer>();
-            while(!nodeQueue.isEmpty()) {
-                TreeNode node = nodeQueue.removeFirst();
-                if(node.left != null) {
-                    tempQueue.add(node.left);
-                }
-                if(node.right != null) {
-                    tempQueue.add(node.right);
-                }
-                level.add(node.val);
-            }
-            // finish one level
-            levels.add(level);
-            nodeQueue = tempQueue;
+        else return null;
+    }
+
+    // using HashMap for cache
+    public TreeNode buildTree2(int[] preorder, int[] inorder) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        if(preorder.length == 0 || inorder.length == 0) {
+            return null;
         }
-        return levels;
+        HashMap<Integer, Integer> inorderMap = new HashMap<Integer, Integer>();
+        for(int i = 0; i < inorder.length; i++) {
+            inorderMap.put(inorder[i], i);
+        }
+        return buildTree2(preorder, 0, preorder.length - 1, inorderMap, 0, inorder.length - 1);
     }
 }
 
@@ -88,9 +117,9 @@ class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] preorder = {1,2,3};
-        int[] inorder = {3,2,2};
-        TreeNode root = solution.buildTree(preorder, inorder);
+        int[] preorder = {1,2};
+        int[] inorder = {1,2};
+        TreeNode root = solution.buildTree2(preorder, inorder);
         print(solution.levelOrder(root));
     }
 }

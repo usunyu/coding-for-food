7b56fbd987ffb274302ea8ce8d932441e2d80b91
usunyu <usunyu@gmail.com>
2013-12-07 16:27:49 -0800

    Longest Consecutive Sequence merge

--- LongestConsecutiveSequence.java
+++ LongestConsecutiveSequence.java
@@ -41,12 +41,38 @@ class Solution {
         }
         return longest;
     }
+
+    private int merge(HashMap<Integer, Integer> map, int left, int right) {
+        int upper = right + map.get(right) - 1;
+        int lower = left - map.get(left) + 1;
+        int len = upper - lower + 1;
+        map.put(upper, len);
+        map.put(lower, len);
+        return len;
+    }
+
+    // cluster merge
+    public int longestConsecutive2(int[] num) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int max = 1;
+        for(int i : num) {
+            if(map.containsKey(i)) continue;
+            map.put(i, 1);
+            if(map.containsKey(i - 1)) {
+                max = Math.max(max, merge(map, i - 1, i));
+            }
+            if(map.containsKey(i + 1)) {
+                max = Math.max(max, merge(map, i, i + 1));
+            }
+        }
+        return max;
+    }
 }
 
 class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] num = {100, 4, 200, 1, 3, 2};
-        System.out.println(solution.longestConsecutive(num));
+        System.out.println(solution.longestConsecutive2(num));
     }
 }

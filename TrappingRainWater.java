f978373b7a1e8797911567c03a664766615ef329
usunyu <usunyu@gmail.com>
2014-01-11 11:49:33 -0800

    Trapping Rain Water

--- TrappingRainWater.java
+++ TrappingRainWater.java
@@ -48,12 +48,72 @@ class Solution {
         }
         return V;
     }
+
+    public int trap2(int[] A) {
+        if(A.length == 0) return 0;
+        // find left
+        int left = 1, prev = A[0];
+        while(left < A.length) {
+            if(A[left] >= prev) {
+                prev = A[left++];
+            }
+            else break;
+        }
+        left--;
+        // scan
+        int V = 0;
+        while(left < A.length) {
+            int current = left + 1;
+            int v = 0;
+            while(current < A.length) {
+                if(A[current] >= A[left]) {
+                    V += v;
+                    break;
+                }
+                else {
+                    v += A[left] - A[current];
+                }
+                current++;
+            }
+            if(current == A.length) {
+                // reverse scan
+                int right = A.length - 2;
+                // find right
+                prev = A[A.length - 1];
+                while(right > left) {
+                    if(A[right] >= prev) {
+                        prev = A[right--];
+                    }
+                    else break;
+                }
+                right++;
+                while(right > left) {
+                    current = right - 1;
+                    v = 0;
+                    while(current > left) {
+                        if(A[current] >= A[right]) {
+                            break;
+                        }
+                        else {
+                            v = A[right] - A[current];
+                            V += v;
+                        }
+                        current--;
+                    }
+                    right = current;
+                }
+                break;
+            }
+            left = current;
+        }
+        return V;
+    }
 }
 
 class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] A = {5,3,7,7};
-        System.out.println(solution.trap(A));
+        int[] A = {0,7,1,4,6};
+        System.out.println(solution.trap2(A));
     }
 }

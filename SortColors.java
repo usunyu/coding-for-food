3c1c3dd8c38661c30d1845b3517f67cac3d9ecb4
usunyu <usunyu@gmail.com>
2014-01-12 23:44:03 -0800

    Sort Colors one-pass

--- SortColors.java
+++ SortColors.java
@@ -13,6 +13,30 @@ class Solution {
             current = i;
         }
     }
+
+    private void swap(int[] A, int i1, int i2) {
+        int temp = A[i1];
+        A[i1] = A[i2];
+        A[i2] = temp;
+    }
+
+    // one-pass solution
+    public void sortColors2(int[] A) {
+        int pRed = 0, pBlue = A.length - 1, pCurrent = 0;
+        while(pCurrent <= pBlue) {
+            switch (A[pCurrent]) {
+                case 0: // red
+                swap(A, pRed++, pCurrent++);
+                break;
+                case 1: // white
+                pCurrent++;
+                break;
+                case 2: // blue
+                swap(A, pBlue--, pCurrent);
+                break;
+            }
+        }
+    }
 }
 
 class Main {
@@ -27,7 +51,7 @@ class Main {
         Solution solution = new Solution();
         int[] A = {0,1,1,0,0,2,0,2,0,1};
         print(A);
-        solution.sortColors(A);
-        print(A);S
+        solution.sortColors2(A);
+        print(A);
     }
 }

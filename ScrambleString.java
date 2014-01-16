bf0486b74bfa870881afcc8aa3f1040a694dad58
usunyu <usunyu@gmail.com>
2014-01-15 22:17:34 -0800

    Scramble String DP solution

--- ScrambleString.java
+++ ScrambleString.java
@@ -43,6 +43,7 @@ class Solution {
         return true;
     }
     
+    // recursion solution
     public boolean isScramble(String s1, String s2) {
         // check length
         if(s1.length() != s2.length()) return false;
@@ -62,6 +63,39 @@ class Solution {
         if(!isSameChars(charMap)) return false;
         // check scramble
         return isScrambleRec(s1, s2, charMap);
+    }
+
+    // DP solution
+    public boolean isScramble2(String s1, String s2) {  
+        int len = s1.length();
+        if(len != s2.length()) return false;
+        if (s1.equals(s2)) return true;
+
+        // a table of matches
+        // T[i][j][k] = true iff s2.substring(j,j+k+1) is a scambled string of s1.substring(i,i+k+1)
+        boolean[][][] scrambled = new boolean[len][len][len];
+        for (int i=0; i < len; ++i) {
+            for (int j=0; j < len; ++j) {
+                scrambled[i][j][0] = (s1.charAt(i) == s2.charAt(j));
+            }
+        }
+
+        // dynamically fill up the table
+        for (int k=1; k < len; ++k) { // k: length
+            for (int i=0; i < len - k; ++i) { // i: index in s1
+                for (int j=0; j < len - k; ++j) { // j: index in s2
+                    // scrambled[i][j][k] = false;
+                    for (int p=0; p < k; ++p) { // p: split into [0..p] and [p+1..k]
+                        if ((scrambled[i][j][p] && scrambled[i+p+1][j+p+1][k-p-1])
+                            || (scrambled[i][j+k-p][p] && scrambled[i+p+1][j][k-p-1])) {
+                            scrambled[i][j][k] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return scrambled[0][0][len-1];
     }
 }
 

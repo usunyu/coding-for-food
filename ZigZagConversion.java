8c5a750b1afffc289ce6aed919afd68f933252a3
Sun Yu <usunyu@gmail.com>
2014-01-26 12:02:44 -0800

    ZigZag Conversion

---
+++ ZigZagConversion.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String convert(String s, int nRows) {
+        if(nRows == 1) return s;
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0; i < nRows; i++) {
+            if(i == 0 || i == nRows - 1) {
+                for(int j = i; j < s.length(); j+= (2 * nRows - 2)) {
+                    builder.append(s.charAt(j));
+                }
+            }
+            else {
+                for(int j = i; j < s.length(); j+= (2 * nRows - 2)) {
+                    builder.append(s.charAt(j));
+                    int block = j / (2 * nRows - 2) + 1;
+                    int k = block * (2 * nRows - 2) - i;
+                    if(k < s.length())
+                        builder.append(s.charAt(k));
+                }
+            }
+        }
+        return builder.toString();
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.convert("PAYPALISHIRING", 4));
+    }
+}

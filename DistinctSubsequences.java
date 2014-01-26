59e350554ffdab603b54848648863aa81f2da11d
Sun Yu <usunyu@gmail.com>
2014-01-25 21:30:33 -0800

    Distinct Subsequences DP2

--- DistinctSubsequences.java
+++ DistinctSubsequences.java
@@ -37,11 +37,42 @@ class Solution {
         }
         return dpTable[T.length() - 1][S.length() - 1];
     }
+
+    // DP solution 2
+    // space complexity : O(n)
+    public int numDistinct3(String S, String T) {
+        if(T.length() == 0) return 1;
+        if(S.length() < T.length()) return 0;
+        int[] dpTable = new int[S.length()];
+        // initial
+        for(int i = 0; i < S.length(); i++) {
+            if(S.charAt(i) == T.charAt(0)) {
+                if(i > 0) dpTable[i] = dpTable[i - 1] + 1;
+                else dpTable[i] = 1;
+            }
+            else {
+                if(i > 0) dpTable[i] = dpTable[i - 1];
+            }
+        }
+        for(int i = 1; i < T.length(); i++) {
+            int last = dpTable[i - 1];
+            for(int j = i; j < S.length(); j++) {
+                int tmp = dpTable[j];
+                if(i == j) dpTable[j] = 0;
+                else dpTable[j] = dpTable[j - 1];
+                if(S.charAt(j) == T.charAt(i)) {
+                    dpTable[j] += last;
+                }
+                last = tmp;
+            }
+        }
+        return dpTable[S.length() - 1];
+    }
 }
 
 class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.numDistinct2("rabbbit", "rabbit"));
+        System.out.println(solution.numDistinct3("rabbbit", "rabbit"));
     }
 }

7f06a3520f98a625ccd0dc0422f545d3dca330d4
Yu Sun <Yu.Sun@citygridmedia.com>
2014-02-02 12:52:50 -0800

    Maximum Depth of Binary Tree

---
+++ MaximumDepthofBinaryTree.java
@@ -0,0 +1,26 @@
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode node1 = new TreeNode(2);
+        root.right = node1;
+        TreeNode node2 = new TreeNode(3);
+        node1.left = node2;
+        Solution solution = new Solution();
+        System.out.println(solution.maxDepth(root));
+    }
+}

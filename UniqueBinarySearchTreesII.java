1b69ff7454bf90a5423b736ecc61eac534ae3125
Yu Sun <Yu.Sun@citygridmedia.com>
2014-02-01 21:32:34 -0800

    Unique Binary Search Trees II cache

--- UniqueBinarySearchTreesII.java
+++ UniqueBinarySearchTreesII.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 class TreeNode {
     int val;
@@ -34,11 +35,45 @@ class Solution {
     public ArrayList<TreeNode> generateTrees(int n) {
         return genSubTrees(1, n);
     }
+
+    private String getStrId(int l, int r) {
+        return l + ":" + r;
+    }
+
+    // add cache
+    private ArrayList<TreeNode> genSubTrees2(int l, int r, HashMap<String, ArrayList<TreeNode>> cache) {
+        String strId = getStrId(l, r);
+        if(cache.containsKey(strId)) return cache.get(strId);
+        ArrayList<TreeNode> trees = new ArrayList<TreeNode>();
+        if(r < l) {
+            trees.add(null);
+        }
+        else {
+            for(int i = l; i <= r; i++) {
+                ArrayList<TreeNode> lefts = genSubTrees2(l, i - 1, cache);
+                ArrayList<TreeNode> rights = genSubTrees2(i + 1, r, cache);
+                for(TreeNode left : lefts) {
+                    for(TreeNode right : rights) {
+                        TreeNode root = new TreeNode(i);
+                        root.left = left;
+                        root.right = right;
+                        trees.add(root);
+                    }
+                }
+            }
+        }
+        cache.put(strId, trees);
+        return trees;
+    }
+
+    public ArrayList<TreeNode> generateTrees2(int n) {
+        return genSubTrees2(1, n, new HashMap<String, ArrayList<TreeNode>>());
+    }
 }
 
 class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.generateTrees(3).size());
+        System.out.println(solution.generateTrees2(10).size());
     }
 }

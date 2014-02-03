5dc8c12c565b1511aca7cfd2ed24a6db487879c5
Yu Sun <Yu.Sun@citygridmedia.com>
2014-02-02 20:40:54 -0800

    Triangle O(n) space

--- Triangle.java
+++ Triangle.java
@@ -24,8 +24,31 @@ class Solution {
         return ret;
     }
     
+    // space complexity : O(n^2)
     public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
         return minSubTotal(triangle, 0, 0, new HashMap<String, Integer>());
+    }
+
+    private int minSubTotal2(ArrayList<ArrayList<Integer>> triangle, int row, int col, HashMap<Integer, Integer> cache) {
+    	int ret;
+        if(row == triangle.size() - 1) {	// if reach bottom
+        	ret = triangle.get(row).get(col);
+        }
+        else {	// pick the min
+        	if(cache.containsKey(row + 1)) {
+				ret = Math.min(cache.get(row + 1), minSubTotal2(triangle, row + 1, col + 1, cache)) + triangle.get(row).get(col);
+        	}
+        	else {
+        		ret = Math.min(minSubTotal2(triangle, row + 1, col, cache), minSubTotal2(triangle, row + 1, col + 1, cache)) + triangle.get(row).get(col);
+        	}
+        }
+        cache.put(row, ret);
+        return ret;
+    }
+
+    // space complexity : O(n)
+    public int minimumTotal2(ArrayList<ArrayList<Integer>> triangle) {
+        return minSubTotal2(triangle, 0, 0, new HashMap<Integer, Integer>());
     }
 }
 
@@ -38,6 +61,6 @@ class Main {
         ArrayList<Integer> row3 = new ArrayList<Integer>(); row3.add(6); row3.add(5); row3.add(7);
         ArrayList<Integer> row4 = new ArrayList<Integer>(); row4.add(4); row4.add(1); row4.add(8); row4.add(3);
         triangle.add(row1); triangle.add(row2); triangle.add(row3); triangle.add(row4);
-        System.out.println(solution.minimumTotal(triangle));
+        System.out.println(solution.minimumTotal2(triangle));
     }
 }

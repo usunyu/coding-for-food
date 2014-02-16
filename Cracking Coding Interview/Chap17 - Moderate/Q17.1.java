cf2a6a5c3c03677f5049d3642e06cdb89a511c07
Yu Sun <Yu.Sun@citygridmedia.com>
2014-02-16 13:59:53 -0800

    CTCI Q17.1 sol

--- Cracking Coding Interview/Chap17 - Moderate/Q17.1.java
+++ Cracking Coding Interview/Chap17 - Moderate/Q17.1.java
@@ -2,13 +2,25 @@ class Main {
 	public static void main(String[] args) {
 		int i1 = 10, i2 = 5;
 		System.out.println("i1 = " + i1 + ", i2 = " + i2);
+		// my first sol
 		i2 += i1;
 		i1 = i2 - i1;
 		i2 -= i1;
 		System.out.println("i1 = " + i1 + ", i2 = " + i2);
+		// using bit
 		i2 = i1 | i2;
 		i1 = i2 ^ i1;
 		i2 = i2 ^ i1;
+		System.out.println("i1 = " + i1 + ", i2 = " + i2);
+		// subtract first, avoid overflow
+		i1 = i1 - i2;
+		i2 = i2 + i1;
+		i1 = i2 - i1;
+		System.out.println("i1 = " + i1 + ", i2 = " + i2);
+		// using bit
+		i1 = i1 ^ i2;
+		i2 = i2 ^ i1;
+		i1 = i2 ^ i1;
 		System.out.println("i1 = " + i1 + ", i2 = " + i2);
 	}
 }

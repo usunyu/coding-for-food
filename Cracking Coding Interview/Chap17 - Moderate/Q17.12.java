13d3744c831ea189c196caca992f3c5b0c7ce207
Yu Sun <Yu.Sun@citygridmedia.com>
2014-02-18 21:22:04 -0800

    CTCI Q17.2

---
+++ Cracking Coding Interview/Chap17 - Moderate/Q17.12.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+class Main {
+	// time: O(nlogn)
+	// space: O(1)
+	public static void twoSum(int[] array, int target) {
+		// sort
+		Arrays.sort(array);
+		for(int i = 0, j = array.length - 1; i < j;) {
+			int sum = array[i] + array[j];
+			if(sum == target) {
+				System.out.print("[" + array[i] + ", " + array[j] + "]");
+				i++; j--;
+			}
+			else if(sum < target) {
+				i++;
+			}
+			else {
+				j--;
+			}
+		}
+		System.out.println();
+	}
+
+	// time: O(n)
+	// space: O(n)
+	public static void twoSum2(int[] array, int target) {
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for(int i : array) {
+			if(map.containsKey(i)) {
+				map.put(i, map.get(i) + 1);
+			}
+			else {
+				map.put(i, 1);
+			}
+		}
+		for(int i : array) {
+			if(map.containsKey(i)) {
+				int need = target - i;
+				if(map.containsKey(need)) {
+					System.out.print("[" + i + ", " + need + "]");
+					if(map.get(i) == 1) {
+						map.remove(i);
+					}
+					else {
+						map.put(i, map.get(i) - 1);
+					}
+					if(map.get(need) == 1) {
+						map.remove(need);
+					}
+					else {
+						map.put(need, map.get(need) - 1);
+					}
+				}
+			}
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		int[] array = {4,1,6,3,8,2,4,2,7,3,5};
+		twoSum(array, 6);
+		twoSum2(array, 6);
+	}
+}

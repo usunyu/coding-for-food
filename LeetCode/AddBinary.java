02c284c8e15d2cf864bdcdb19e046753d0db3952
Yu Sun <Yu.Sun@citygridmedia.com>
2014-02-26 18:03:59 -0800

    Add Binary

--- LeetCode/AddBinary.java
+++ LeetCode/AddBinary.java
@@ -1,3 +1,11 @@
+/*
+Given two binary strings, return their sum (also a binary string).
+
+For example,
+a = "11"
+b = "1"
+Return "100".
+*/
 
 class Solution {
     // time complexity : O(N)
@@ -54,6 +62,43 @@ class Solution {
         strBuff.reverse();
         
         return strBuff.toString();
+    }
+
+    /*
+        Second Round
+    */
+    public String addBinary2(String a, String b) {
+        if(a == null || b == null || (a.length() == 0 && b.length() == 0)) return null;
+        if(a.length() == 0) return b;
+        if(b.length() == 0) return a;
+        StringBuilder sb = new StringBuilder();
+        int i, carry = 0;
+        // add
+        for(i = 0; i < a.length() && i < b.length(); i++) {
+            int an = a.charAt(a.length() - 1 - i) - '0';
+            int bn = b.charAt(b.length() - 1 - i) - '0';
+            int res = an + bn + carry;
+            sb.insert(0, res % 2);
+            carry = res / 2;
+        }
+        // finish left
+        while(i < a.length()) {
+            int an = a.charAt(a.length() - 1 - i) - '0';
+            int res = an + carry;
+            sb.insert(0, res % 2);
+            carry = res / 2;
+            i++;
+        }
+        while(i < b.length()) {
+            int bn = b.charAt(b.length() - 1 - i) - '0';
+            int res = bn + carry;
+            sb.insert(0, res % 2);
+            carry = res / 2;
+            i++;
+        }
+        // add last carry
+        if(carry != 0) sb.insert(0, carry);
+        return sb.toString();
     }
 }
 

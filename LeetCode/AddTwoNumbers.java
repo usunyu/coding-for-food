0c54e644ae0891909f8e7a1e4c7f039b6da4383a
Yu Sun <Yu.Sun@citygridmedia.com>
2014-02-26 18:21:52 -0800

    Add Two Numbers

--- LeetCode/AddTwoNumbers.java
+++ LeetCode/AddTwoNumbers.java
@@ -1,3 +1,11 @@
+/*
+You are given two linked lists representing two non-negative numbers.
+The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+*/
+
 import java.util.*;
 
 class ListNode {
@@ -50,12 +58,94 @@ class Solution {
         }
         return head;
     }
+
+    /*
+        Second Round
+    */
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        ListNode p1 = l1, p2 = l2, head = null, prev = null;
+        int carry = 0;
+        // add
+        while(p1 != null && p2 != null) {
+            int val = p1.val + p2.val + carry;
+            ListNode node = new ListNode(val % 10);
+            carry = val / 10;
+            if(head == null) {
+                head = node;
+                prev = node;
+            }
+            else {
+                prev.next = node;
+                prev = node;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        // finish left
+        while(p1 != null) {
+            int val = p1.val + carry;
+            ListNode node = new ListNode(val % 10);
+            carry = val / 10;
+            if(head == null) {
+                head = node;
+                prev = node;
+            }
+            else {
+                prev.next = node;
+                prev = node;
+            }
+            p1 = p1.next;
+        }
+        while(p2 != null) {
+            int val = p2.val + carry;
+            ListNode node = new ListNode(val % 10);
+            carry = val / 10;
+            if(head == null) {
+                head = node;
+                prev = node;
+            }
+            else {
+                prev.next = node;
+                prev = node;
+            }
+            p2 = p2.next;
+        }
+        if(carry != 0) {
+            ListNode node = new ListNode(carry);
+            if(prev != null) prev.next = node;
+            else head = node;
+        }
+        return head;
+    }
 }
 
 class Main {
+    public static void print(ListNode head) {
+        while(head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
-        
+
+        ListNode head1 = new ListNode(2);
+        ListNode node1 = new ListNode(4);
+        head1.next = node1;
+        ListNode node2 = new ListNode(3);
+        node1.next = node2;
+
+        ListNode head2 = new ListNode(5);
+        ListNode node3 = new ListNode(6);
+        head2.next = node3;
+        ListNode node4 = new ListNode(4);
+        node3.next = node4;
+
+        print(head1);
+        print(head2);
+        print(solution.addTwoNumbers2(head1, head2));
     }
 }
 

c7433c5687468202da56fdad113f4aef376d2c88
Yu Sun <Yu.Sun@citygridmedia.com>
2014-03-01 01:18:03 -0800

    Best Time to Buy and Sell Stock III

--- LeetCode/BestTimetoBuyandSellStockIII.java
+++ LeetCode/BestTimetoBuyandSellStockIII.java
@@ -1,3 +1,12 @@
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete at most two transactions.
+
+Note:
+You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+*/
+
 class Solution {
     public int maxProfit(int[] prices) {
         if(prices.length == 0) return 0;
@@ -30,12 +39,44 @@ class Solution {
         }
         return maxProfit;
     }
+
+    /*
+        Second Round
+    */
+    public int maxProfit2(int[] prices) {
+        int start = 0, length = prices.length;
+        if(length == 0) return 0;
+        int[] profitHistory = new int[length];
+        // scan forward for profit history
+        for(int i = 1; i < length; i++) {
+            int profit = prices[i] - prices[start];
+            if(profit >= 0) {
+                profitHistory[i] = Math.max(profitHistory[i - 1], profit);
+            }
+            else {
+                profitHistory[i] = profitHistory[i - 1];
+                start = i;
+            }
+        }
+        // scan back for two trans
+        int end = length - 1, maxProfit = profitHistory[length - 1];
+        for(int i = length - 2; i > 0; i--) {
+            int profit = prices[end] - prices[i];
+            if(profit >= 0) {
+                maxProfit = Math.max(maxProfit, profit + profitHistory[i - 1]);
+            }
+            else {
+                end = i;
+            }
+        }
+        return maxProfit;
+    }
 }
 
 class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] prices = {1,2,4,2,5,7,2,4,9,0};
-        System.out.println(solution.maxProfit(prices));
+        System.out.println(solution.maxProfit2(prices));
     }
 }

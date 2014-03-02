9f9069a9dc29aee616876b68953d9789b7f78076
Yu Sun <Yu.Sun@citygridmedia.com>
2014-03-01 23:57:57 -0800

    Binary Tree Level Order Traversal

--- LeetCode/BinaryTreeLevelOrderTraversal.java
+++ LeetCode/BinaryTreeLevelOrderTraversal.java
@@ -1,3 +1,37 @@
+/*
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
+
+OJ's Binary Tree Serialization:
+The serialization of a binary tree follows a level order traversal, where '#' signifies a path terminator where no node exists below.
+
+Here's an example:
+   1
+  / \
+ 2   3
+    /
+   4
+    \
+     5
+The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}".
+*/
+
 import java.util.*;
 
 // Definition for binary tree
@@ -38,6 +72,26 @@ class Solution {
         }
         return levels;
     }
+
+    public ArrayList<ArrayList<Integer>> levelOrder2(TreeNode root) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        if(root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            Queue<TreeNode> tmp = new LinkedList<TreeNode>();
+            ArrayList<Integer> sub = new ArrayList<Integer>();
+            while(!queue.isEmpty()) {
+                TreeNode node = queue.poll();
+                sub.add(node.val);
+                if(node.left != null) tmp.add(node.left);
+                if(node.right != null) tmp.add(node.right);
+            }
+            result.add(sub);
+            queue = tmp;
+        }
+        return result;
+    }
 }
 
 class Main {
@@ -58,7 +112,7 @@ class Main {
         TreeNode right = new TreeNode(20);
         root.left = left;
         root.right = right;
-        print(solution.levelOrder(root));
+        print(solution.levelOrder2(root));
     }
 }
 

6bd7ff37fde15a8a17154f124c31b62dc992a4c6
Yu Sun <Yu.Sun@citygridmedia.com>
2014-03-01 23:11:30 -0800

    Binary Tree Inorder Traversal

--- LeetCode/BinaryTreeInorderTraversal.java
+++ LeetCode/BinaryTreeInorderTraversal.java
@@ -1,3 +1,33 @@
+/*
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+For example:
+Given binary tree {1,#,2,3},
+   1
+    \
+     2
+    /
+   3
+return [1,3,2].
+
+Note: Recursive solution is trivial, could you do it iteratively?
+
+confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
+
+OJ's Binary Tree Serialization:
+The serialization of a binary tree follows a level order traversal, where '#' signifies a path terminator where no node exists below.
+
+Here's an example:
+   1
+  / \
+ 2   3
+    /
+   4
+    \
+     5
+The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}".
+*/
+
 import java.util.*;
 
 class TreeNode {
@@ -42,6 +72,30 @@ class Solution {
         }
         return result;
     }
+
+    /*
+        Second Round
+    */
+    public ArrayList<Integer> inorderTraversal3(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if(root == null) return result;
+        TreeNode ptr = root;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        while(ptr != null || !stack.isEmpty()) {
+            if(ptr != null) {
+                while(ptr != null) {
+                    stack.push(ptr);
+                    ptr = ptr.left;
+                }
+            }
+            else {
+                TreeNode node = stack.pop();
+                result.add(node.val);
+                ptr = node.right;
+            }
+        }
+        return result;
+    }
 }
 
 class Main {
@@ -52,7 +106,7 @@ class Main {
         TreeNode node2 = new TreeNode(3);
         node1.left = node2;
         Solution solution = new Solution();
-        ArrayList<Integer> result = solution.inorderTraversal2(root);
+        ArrayList<Integer> result = solution.inorderTraversal3(root);
         for(int i : result) {
             System.out.print(i + " ");
         }

4cde6192a2d25224d487e896774304775cde2450
Yu Sun <Yu.Sun@citygridmedia.com>
2014-03-02 16:37:08 -0800

    Binary Tree Maximum Path Sum

--- LeetCode/BinaryTreeMaximumPathSum.java
+++ LeetCode/BinaryTreeMaximumPathSum.java
@@ -1,3 +1,17 @@
+/*
+Given a binary tree, find the maximum path sum.
+
+The path may start and end at any node in the tree.
+
+For example:
+Given the below binary tree,
+
+       1
+      / \
+     2   3
+Return 6.
+*/
+
 import java.util.*;
 
 class TreeNode {
@@ -70,6 +84,49 @@ class Solution {
         data.sum = Math.max(Math.max(l.sum, r.sum), l.path+root.val+r.path);
         return data;
     }
+
+    /*
+        Second Round
+    */
+
+    static int max = Integer.MIN_VALUE;
+    
+    private int pathSum(TreeNode root) {
+        if(root == null) return 0;
+        int left = pathSum(root.left);
+        int right = pathSum(root.right);
+        int current = left + right + root.val;
+        max = Math.max(max, current);
+        return Math.max(0, current + Math.max(left, right));
+    }
+    
+    // don't know why cannot pass {-3} in leetcode, WTF!!
+    public int maxPathSum3(TreeNode root) {  
+        if(root == null) return 0;
+        pathSum(root);
+        return max;
+    }
+
+    class Pair {
+        int sum;
+        int max = Integer.MIN_VALUE;
+    }
+    
+    private Pair pathSum2(TreeNode root) {
+        Pair pair = new Pair();
+        if(root == null) return pair;
+        Pair left = pathSum2(root.left);
+        Pair right = pathSum2(root.right);
+        pair.max = Math.max(Math.max(left.max, right.max), root.val + left.sum + right.sum);
+        pair.sum = Math.max(0, Math.max(left.sum, right.sum) + root.val);
+        return pair;
+    }
+    
+    public int maxPathSum4(TreeNode root) {  
+        if(root == null) return 0;
+        Pair pair = pathSum2(root);
+        return pair.max;
+    }
 }
 
 class Main {
@@ -80,6 +137,6 @@ class Main {
         root.left = t1;
         TreeNode t2 = new TreeNode(3);
         root.right = t2;
-        System.out.println(solution.maxPathSum2(root));
+        System.out.println(solution.maxPathSum4(root));
     }
 }

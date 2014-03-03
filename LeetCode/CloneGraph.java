91674c5bd94c5b9c7741a3417e39d7e39d08a992
Yu Sun <Yu.Sun@citygridmedia.com>
2014-03-03 12:01:03 -0800

    Clone Graph

--- LeetCode/CloneGraph.java
+++ LeetCode/CloneGraph.java
@@ -1,3 +1,28 @@
+/*
+Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
+
+
+OJ's undirected graph serialization:
+Nodes are labeled uniquely.
+
+We use # as a separator for each node, and , as a separator for node label and each neighbor of the node.
+As an example, consider the serialized graph {0,1,2#1,2#2,2}.
+
+The graph has a total of three nodes, and therefore contains three parts as separated by #.
+
+First node is labeled as 0. Connect node 0 to both nodes 1 and 2.
+Second node is labeled as 1. Connect node 1 to node 2.
+Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a self-cycle.
+Visually, the graph looks like the following:
+
+       1
+      / \
+     /   \
+    0 --- 2
+         / \
+         \_/
+*/
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -27,6 +52,26 @@ class Solution {
         if(node == null) return null;
         HashMap<UndirectedGraphNode, UndirectedGraphNode> nodeMap = new HashMap<UndirectedGraphNode, UndirectedGraphNode>();
         return cloneGraph(node, nodeMap);
+    }
+
+    private UndirectedGraphNode cloneGraph2(UndirectedGraphNode node, HashMap<UndirectedGraphNode, UndirectedGraphNode> map) {
+        if(map.containsKey(node)) {
+            return map.get(node);
+        }
+        else {
+            UndirectedGraphNode cloneNode = new UndirectedGraphNode(node.label);
+            map.put(node, cloneNode);
+            for(UndirectedGraphNode neighbor : node.neighbors) {
+                cloneNode.neighbors.add(cloneGraph2(neighbor, map));
+            }
+            return cloneNode;
+        }
+    }
+    
+    public UndirectedGraphNode cloneGraph2(UndirectedGraphNode node) {
+        if(node == null) return null;
+        HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<UndirectedGraphNode, UndirectedGraphNode>();
+        return cloneGraph2(node, map);
     }
 }
 

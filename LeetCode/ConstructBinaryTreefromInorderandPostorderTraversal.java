4243c70dd5a9ca7213bf1837c45e01bfa6f018ec
Yu Sun <Yu.Sun@citygridmedia.com>
2014-03-04 10:07:26 -0800

    Construct Binary Tree from Inorder and Postorder Traversal

--- LeetCode/ConstructBinaryTreefromInorderandPostorderTraversal.java
+++ LeetCode/ConstructBinaryTreefromInorderandPostorderTraversal.java
@@ -1,3 +1,10 @@
+/*
+Given inorder and postorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+*/
+
 import java.util.*;
 
 class TreeNode {
@@ -57,6 +64,29 @@ class Solution {
         for(int i = 0; i < inorder.length; i++) inorderMap.put(inorder[i], i);
         return buildTree(postorder, 0, postorder.length - 1, inorderMap, 0);
     }
+
+    /*
+        Second Round
+    */
+    private TreeNode buildTree2(int[] inorder, int istart, int iend, int[] postorder, int pstart, int pend) {
+        if(istart > iend || pstart > pend) return null;
+        TreeNode root = new TreeNode(postorder[pend]);
+        // search index need split in inorder
+        int index;
+        for(index = istart; index <= iend; index++) {
+            if(inorder[index] == postorder[pend]) break;
+        }
+        index -= istart;
+        root.left = buildTree2(inorder, istart, istart + index - 1, postorder, pstart, pstart + index - 1);
+        root.right = buildTree2(inorder, istart + index + 1, iend, postorder, pstart + index, pend - 1);
+        return root;
+    }
+    
+    public TreeNode buildTree2(int[] inorder, int[] postorder) {
+        if(inorder.length == 0 || inorder.length != postorder.length) return null;
+        int length = inorder.length;
+        return buildTree2(inorder, 0, length - 1, postorder, 0, length - 1);
+    }
 }
 
 class Main {
@@ -74,7 +104,7 @@ class Main {
         Solution solution = new Solution();
         int[] inorder = {4,2,5,1,6,3,7};
         int[] postorder = {4,5,2,6,7,3,1};
-        TreeNode root = solution.buildTree(inorder, postorder);
+        TreeNode root = solution.buildTree2(inorder, postorder);
         print(solution.levelOrder(root));
     }
 }

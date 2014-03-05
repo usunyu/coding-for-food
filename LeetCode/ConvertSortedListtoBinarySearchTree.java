63db454b86049c86c31496cb647c885a51d52884
Yu Sun <Yu.Sun@citygridmedia.com>
2014-03-04 17:25:32 -0800

    Convert Sorted List to Binary Search Tree

--- LeetCode/ConvertSortedListtoBinarySearchTree.java
+++ LeetCode/ConvertSortedListtoBinarySearchTree.java
@@ -71,20 +71,20 @@ class Solution {
 
     // time complexity : O(N)
     // space complexity : O(1)
-    private TreeNode listToBST(ListNode head, int low, int high) {  
-        if (low > high) return null;  
-        int mid = low + (high - low) / 2;  
-        // build up tree recursively  
-        TreeNode left = listToBST(head, low, mid-1);  
-        TreeNode root = new TreeNode(head.val);  
-        root.left = left;  
-        // Java pass in Object by reference, so we can't change head but we can change its content :)  
-        if (head.next != null) { // "move to next"  
+    private TreeNode listToBST(ListNode head, int low, int high) {
+        if (low > high) return null;
+        int mid = low + (high - low) / 2;
+        // build up tree recursively
+        TreeNode left = listToBST(head, low, mid-1);
+        TreeNode root = new TreeNode(head.val);
+        root.left = left;
+        // Java pass in Object by reference, so we can't change head but we can change its content :)
+        if (head.next != null) { // "move to next"
             head.val = head.next.val;
             head.next = head.next.next;
             root.right = listToBST(head, mid+1, high);
         }
-        return root;  
+        return root;
     }
 
     // http://n00tc0d3r.blogspot.com/search?q=Convert+Sorted+List+to+Binary+Search+Tree
@@ -97,6 +97,34 @@ class Solution {
         }  
         // build the BST  
         return listToBST(head, 0, len-1);  
+    }
+
+    /*
+        Second Round
+    */
+    private TreeNode buildBST(ListNode head, int left, int right) {
+        if(left > right) return null;
+        int mid = left + (right - left) / 2;
+        // find mid node
+        ListNode node = head;
+        for(int i = left; i < mid; i++) {
+            node = node.next;
+        }
+        TreeNode root = new TreeNode(node.val);
+        root.left = buildBST(head, left, mid - 1);
+        root.right = buildBST(node.next, mid + 1, right);
+        return root;
+    }
+    
+    public TreeNode sortedListToBST3(ListNode head) {
+        if(head == null) return null;
+        int length = 0;
+        ListNode node = head;
+        while(node != null) {
+            node = node.next;
+            length++;
+        }
+        return buildBST(head, 0, length - 1);
     }
 }
 
@@ -124,7 +152,7 @@ class Main {
             }
             prev = node;
         }
-        TreeNode root = solution.sortedListToBST2(head);
+        TreeNode root = solution.sortedListToBST3(head);
         print(solution.levelOrder(root));
     }
 }

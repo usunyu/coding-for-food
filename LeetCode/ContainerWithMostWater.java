eecdd2963c8570a3d8b4482a5337fe579dbd2fb4
Yu Sun <Yu.Sun@citygridmedia.com>
2014-03-04 16:11:58 -0800

    Container With Most Water

--- LeetCode/ContainerWithMostWater.java
+++ LeetCode/ContainerWithMostWater.java
@@ -1,3 +1,11 @@
+/*
+Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai).
+n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines,
+which together with x-axis forms a container, such that the container contains the most water.
+
+Note: You may not slant the container.
+*/
+
 class Solution {
     // Time Limit Exceeded
     public int maxArea(int[] height) {
@@ -41,12 +49,28 @@ class Solution {
         }
         return maxArea;
     }
+
+    /*
+        Second Round
+    */
+    public int maxArea3(int[] height) {
+        if(height.length == 0) return 0;
+        int maxArea = 0;
+        int left = 0, right = height.length - 1;
+        while(left < right) {
+            int area = (right - left) * Math.min(height[left], height[right]);
+            maxArea = Math.max(area, maxArea);
+            if(height[left] > height[right]) right--;
+            else left++;
+        }
+        return maxArea;
+    }
 }
 
 class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] height = {2, 3, 2, 4};
-        System.out.println(solution.maxArea2(height));
+        System.out.println(solution.maxArea3(height));
     }
 }

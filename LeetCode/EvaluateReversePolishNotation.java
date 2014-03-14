a25934bfed25a8904c91c122fc8d140b46927926
Yu Sun <Yu.Sun@citygridmedia.com>
2014-03-13 22:13:34 -0700

    Evaluate Reverse Polish Notation

--- LeetCode/EvaluateReversePolishNotation.java
+++ LeetCode/EvaluateReversePolishNotation.java
@@ -1,3 +1,13 @@
+/*
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+
+Some examples:
+  ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+  ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+*/
+
 import java.util.Stack;
 
 class Solution {
@@ -39,12 +49,47 @@ class Solution {
         }
         return operand.pop();
     }
+    /*
+        Second Round
+    */
+    public int evalRPN2(String[] tokens) {
+        Stack<Integer> stack = new Stack<Integer>();
+        for(String token : tokens) {
+            int op1, op2;
+            switch(token) {
+                case "+":
+                    op1 = stack.pop();
+                    op2 = stack.pop();
+                    stack.push(op2 + op1);
+                    break;
+                case "-":
+                    op1 = stack.pop();
+                    op2 = stack.pop();
+                    stack.push(op2 - op1);
+                    break;
+                case "*":
+                    op1 = stack.pop();
+                    op2 = stack.pop();
+                    stack.push(op2 * op1);
+                    break;
+                case "/":
+                    op1 = stack.pop();
+                    op2 = stack.pop();
+                    stack.push(op2 / op1);
+                    break;
+                default:
+                    stack.push(Integer.parseInt(token));
+                    break;
+            }
+        }
+        return stack.pop();
+    }
 }
 
 class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
         String[] tokens = {"4", "13", "5", "/", "+"};
-        System.out.println((solution.evalRPN(tokens)));
+        System.out.println((solution.evalRPN2(tokens)));
     }
 }

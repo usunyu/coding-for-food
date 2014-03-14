df03c1ec2be9a79a3b5ef1bfee59c64e069ab1dd
Yu Sun <Yu.Sun@citygridmedia.com>
2014-03-13 17:38:13 -0700

    Multiply all fields except it's own position

--- CareerCup/MultiplyAllExceptOwn.java
+++ CareerCup/MultiplyAllExceptOwn.java
@@ -12,6 +12,8 @@ http://www.careercup.com/question?id=5179916190482432
 */
 
 class Main {
+	// break restriction (1)
+	// time : O(n) space : O(1)
 	public static void multiply(int[] input) {
 		int front = 1, end = 1;
 		// get end
@@ -27,6 +29,22 @@ class Main {
 		}
 	}
 
+	// time : O(n) space : O(n)
+	public static void multiply2(int[] input) {
+		int[] product = new int[input.length];
+		int p = 1;
+		for(int i = input.length - 1; i >= 0; i--) {
+			p = p * input[i];
+			product[i] = p;
+		}
+		p = 1;
+		for(int i = 0; i < input.length; i++) {
+			int tmp = p * input[i];
+			input[i] = p * (i < input.length - 1 ? product[i + 1] : 1);
+			p = tmp;
+		}
+	}
+
 	public static void print(int[] array) {
 		for(int i = 0; i < array.length; i++) {
 			System.out.print(array[i] + "\t");
@@ -37,7 +55,7 @@ class Main {
 	public static void main(String[] args) {
 		int[] input = {2,3,1,4};
 		print(input);
-		multiply(input);
+		multiply2(input);
 		print(input);
 	}
 }

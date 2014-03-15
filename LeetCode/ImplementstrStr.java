a64374773325e748dcead056f5c1571033f97f4f
Yu Sun <Yu.Sun@citygridmedia.com>
2014-03-14 22:42:40 -0700

    Implement strStr() KMP

--- LeetCode/ImplementstrStr.java
+++ LeetCode/ImplementstrStr.java
@@ -31,15 +31,55 @@ class Solution {
         }
         return null;
     }
+    /*
+        Second Round
+    */
+    // KMP 
+    // time complexity : O(n)
+    // space complexity : O(m)
+    public String strStr2(String haystack, String needle) {
+        if(needle == null || needle.length() == 0) return haystack;
+        if(haystack == null || haystack.length() < needle.length()) return null;
+        // pre-process
+        int[] table = new int[needle.length()];
+        int i = 1, length = 0;
+        while(i < needle.length()) {
+            if(needle.charAt(i) == needle.charAt(table[length])) {
+                length++;
+                table[i++] = length;
+            }
+            else if(length > 0) {
+                length = table[length - 1];
+            }
+            else {
+                i++;
+            }
+        }
+        // kmp
+        i = 0; length = 0;
+        while(i < haystack.length()) {
+            if(haystack.charAt(i) == needle.charAt(length)) {
+                length++; i++;
+            }
+            else if(length > 0) {
+                length = table[length - 1];
+            }
+            else {
+                i++;
+            }
+            if(length == needle.length()) return haystack.substring(i - length);
+        }
+        return null;
+    }
 }
 
 class Main {
 
     public static void main(String[] args) {
-    	String haystack = "";
-    	String needle = "";
+    	String haystack = "happyappy";
+    	String needle = "app";
         Solution solution = new Solution();
-        String str = solution.strStr(haystack, needle);
+        String str = solution.strStr2(haystack, needle);
         System.out.println(str);
     }
 }

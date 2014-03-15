3fff6b60e88b4e2caf83e6edb8ce3e0098d1ff44
Yu Sun <Yu.Sun@citygridmedia.com>
2014-03-14 19:24:30 -0700

    Gas Station

--- LeetCode/GasStation.java
+++ LeetCode/GasStation.java
@@ -1,3 +1,15 @@
+/*
+There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
+
+You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1). 
+You begin the journey with an empty tank at one of the gas stations.
+
+Return the starting gas station's index if you can travel around the circuit once, otherwise return -1.
+
+Note:
+The solution is guaranteed to be unique.
+*/
+
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int length = 0, total = 0, start = 0;
@@ -20,6 +32,29 @@ class Solution {
         // cannot travel around the circuit
         return -1;
     }
+    /*
+        Second Round
+    */
+    public int canCompleteCircuit2(int[] gas, int[] cost) {
+        int start = 0, total = 0, length = 0;
+        while(start < gas.length) {
+            int i = start + length;
+            while(total >= 0 && length <= gas.length) {
+                // circular
+                i = (i >= gas.length ? i - gas.length : i);
+                length++;
+                total += (gas[i] - cost[i]);
+                i++;
+            }
+            if(length == gas.length + 1) return start;
+            // back
+            total += (cost[start] - gas[start]);
+            length--;
+            // try next one
+            start++;
+        }
+        return -1;
+    }
 }
 
 class Main {
@@ -27,6 +62,6 @@ class Main {
         Solution solution = new Solution();
         int[] gas = {5, 10, 3};
         int[] cost = {6, 5, 1};
-        System.out.println(solution.canCompleteCircuit(gas, cost));
+        System.out.println(solution.canCompleteCircuit2(gas, cost));
     }
 }

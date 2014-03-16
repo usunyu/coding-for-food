23161bc1be8004dbca396f16d7bbd4e945d1d699
Yu Sun <Yu.Sun@citygridmedia.com>
2014-03-15 19:05:15 -0700

    Interleaving String

--- LeetCode/InterleavingString.java
+++ LeetCode/InterleavingString.java
@@ -1,3 +1,15 @@
+/*
+Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
+
+For example,
+Given:
+s1 = "aabcc",
+s2 = "dbbca",
+
+When s3 = "aadbbcbcac", return true.
+When s3 = "aadbbbaccc", return false.
+*/
+
 class Solution {
     // recursion solution
     // Time Limit Exceeded
@@ -33,12 +45,42 @@ class Solution {
         }
         return match[s1.length()][s2.length()];
     }
+    /*
+        Second Round
+    */
+    public boolean isInterleave3(String s1, String s2, String s3) {  
+        int m = s1.length(), n = s2.length(), k = s3.length();
+        if(m + n != k) return false;
+        if(m == 0) return s3.equals(s2);
+        if(n == 0) return s3.equals(s1);
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        // init
+        dp[0][0] = true;
+        for(int i = 1; i <= m; i++) {
+            if(s1.charAt(i - 1) == s3.charAt(i - 1)) dp[i][0] = true;
+            else break;
+        }
+        for(int i = 1; i <= n; i++) {
+            if(s2.charAt(i - 1) == s3.charAt(i - 1)) dp[0][i] = true;
+            else break;
+        }
+        // dp
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)
+                    || dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1))
+                    dp[i][j] = true;
+            }
+        }
+        // ret
+        return dp[m][n];
+    }  
 }
 
 class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
         String s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac";
-        System.out.println(solution.isInterleave2(s1, s2, s3));
+        System.out.println(solution.isInterleave3(s1, s2, s3));
     }
 }

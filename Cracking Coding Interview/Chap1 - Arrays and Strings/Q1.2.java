e4f3574af7f97366b59865aedb53938858aa5660
usunyu <usunyu@gmail.com>
2014-03-16 22:32:19 -0700

    CTCI Q1.2

--- Cracking Coding Interview/Chap1 - Arrays and Strings/Q1.2.java
+++ Cracking Coding Interview/Chap1 - Arrays and Strings/Q1.2.java
@@ -1,4 +1,9 @@
-class Q1_2App {
+/*
+Implement a function void reversefchar* str) in Cor C++ which reverses a null-termi- nated string.
+*/
+
+class Solution {
+	/*
 	public static String reverse(String str) {
 		StringBuffer sb = new StringBuffer(str);
 		for(int i = 0, j = sb.length() - 1; i < j; i++, j--) {
@@ -14,5 +19,26 @@ class Q1_2App {
 		System.out.println(testStr);
 		testStr = reverse(testStr);
 		System.out.println(testStr);
+	}
+	*/
+	/*
+		Second Round
+	*/
+	public static String reverse(String str) {
+		StringBuilder sb = new StringBuilder(str);
+		for(int i = 0, j = sb.length() - 1; i < j; i++, j--) {
+			char tmp = sb.charAt(j);
+			sb.setCharAt(j, sb.charAt(i));
+			sb.setCharAt(i, tmp);
+		}
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+		String[] input = {"abcde", "cat"};
+		for(int i = 0; i < 2; i++) {
+			System.out.println("reversing the string: " + input[i]);
+			System.out.println("reverse of input string is " + reverse(input[i]));
+    	}
 	}
 }

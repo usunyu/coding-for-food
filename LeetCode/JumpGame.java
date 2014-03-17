dcc20500ec20bbd4bba3a2a0a78f69192e08ca86
usunyu <usunyu@gmail.com>
2014-03-16 20:31:16 -0700

    Jump Game

--- LeetCode/JumpGame.java
+++ LeetCode/JumpGame.java
@@ -1,3 +1,16 @@
+/*
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Determine if you are able to reach the last index.
+
+For example:
+A = [2,3,1,1,4], return true.
+
+A = [3,2,1,0,4], return false.
+*/
+
 class Solution {
     private boolean canJump(int[] A, int current) {
         if(current >= A.length - 1) return true;
@@ -22,6 +35,17 @@ class Solution {
             if(A[i] >= (next - i)) {
                 next = i;
             }
+        }
+        return next == 0;
+    }
+    /*
+        Second Round
+    */
+    public boolean canJump3(int[] A) {
+        int next = A.length - 1;
+        for(int i = A.length - 2; i >= 0; i--) {
+            if(A[i] + i >= next) 
+                next = i;
         }
         return next == 0;
     }

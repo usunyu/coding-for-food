2d81679bcb060a2e332a8d0739a95549c8418537
usunyu <usunyu@gmail.com>
2014-03-17 00:47:54 -0700

    Convert Sorted List to Binary Search Tree refactor

--- LeetCode/ConvertSortedListtoBinarySearchTree.java
+++ LeetCode/ConvertSortedListtoBinarySearchTree.java
@@ -3,50 +3,9 @@ Given a singly linked list where elements are sorted in ascending order, convert
 */
 
 import java.util.*;
-
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) { val = x; next = null; }
-}
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int x) { val = x; }
-}
+import LCLibrary.*;
 
 class Solution {
-    // from BinaryTreeLevelOrderTraversal.java for testing
-    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
-        ArrayList<ArrayList<Integer>> levels = new ArrayList<ArrayList<Integer>>();
-        if(root == null) {
-            return levels;
-        }
-        LinkedList<TreeNode> nodeQueue = new LinkedList<TreeNode>();
-        nodeQueue.add(root);
-        while(!nodeQueue.isEmpty()) {
-            LinkedList<TreeNode> tempQueue = new LinkedList<TreeNode>();
-            ArrayList<Integer> level = new ArrayList<Integer>();
-            while(!nodeQueue.isEmpty()) {
-                TreeNode node = nodeQueue.removeFirst();
-                if(node.left != null) {
-                    tempQueue.add(node.left);
-                }
-                if(node.right != null) {
-                    tempQueue.add(node.right);
-                }
-                level.add(node.val);
-            }
-            // finish one level
-            levels.add(level);
-            nodeQueue = tempQueue;
-        }
-        return levels;
-    }
-
     public TreeNode sortedArrayToBST(ArrayList<Integer> array, int start, int end) {
         TreeNode root = null;
         if(start <= end) {
@@ -132,31 +91,11 @@ class Solution {
     }
 }
 
-class Main {
-    public static void print(ArrayList<ArrayList<Integer>> levels) {
-        for(ArrayList<Integer> level : levels) {
-            for(int val : level) {
-                System.out.print(val + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-    
+class Main {    
     public static void main(String[] args) {
         Solution solution = new Solution();
-        ListNode head = null, prev = null;
-        for(int i = 1; i <= 10; i++) {
-            ListNode node = new ListNode(i);
-            if(head == null) {
-                head = node;
-            }
-            else {
-                prev.next = node;
-            }
-            prev = node;
-        }
+        ListNode head = Input.buildSortedList();
         TreeNode root = solution.sortedListToBST3(head);
-        print(solution.levelOrder(root));
+        Output.levelOrderTraversalTree(root);
     }
 }
--- LeetCode/LCLibrary/Input.java
+++ LeetCode/LCLibrary/Input.java
@@ -57,6 +57,24 @@ public class Input {
 	}
 
 	/*
+		1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9 -> 10
+	*/
+	public static ListNode buildSortedList() {
+		ListNode head = null, prev = null;
+        for(int i = 1; i <= 10; i++) {
+            ListNode node = new ListNode(i);
+            if(head == null) {
+                head = node;
+            }
+            else {
+                prev.next = node;
+            }
+            prev = node;
+        }
+        return head;
+	}
+
+	/*
 		   1
 	      / \
 	     /   \

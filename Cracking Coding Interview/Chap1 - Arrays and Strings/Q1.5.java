ea27110edd2076b9feab89fc6bf48755cd40d5ff
Yu Sun <Yu.Sun@citygridmedia.com>
2014-03-17 16:52:56 -0700

    CTCI Q1.5

--- Cracking Coding Interview/Chap1 - Arrays and Strings/Q1.5.java
+++ Cracking Coding Interview/Chap1 - Arrays and Strings/Q1.5.java
@@ -1,6 +1,13 @@
+/*
+Implement a method to perform basic string compression using the counts of repeated characters. 
+For example, the string aabcccccaaa would become a2blc5a3. If the "compressed" string would not 
+become smaller than the original string, your method should return the original string.
+*/
+
 import java.util.*;
 
-class Q1_5App {
+class Solution {
+	/*
 	public static String compression(String str) {
 		if(str == null || str.isEmpty())
 			return str;
@@ -34,4 +41,35 @@ class Q1_5App {
 		testStr = compression(testStr);
 		System.out.println(testStr);
 	}
+	*/
+	/*
+		Second Round
+	*/
+	public static String compression(String str) {
+		if(str == null || str.length() == 0) return str;
+		StringBuffer sb = new StringBuffer();
+		int count = 1;
+		char last = str.charAt(0);
+		sb.append(last);
+		for(int i = 1; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if(ch == last) count++;
+			else {
+				sb.append(count);
+				sb.append(ch);
+				last = ch;
+				count = 1;
+			}
+			if(i == str.length() - 1) sb.append(count);
+		}
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+		String str = "abbccccccde";
+		String str2 = compression(str);
+		System.out.println("Old String (len = " + str.length() + "): " + str);
+		System.out.println("New String (len = " + str2.length() + "): " + str2);
+	}
 }
+

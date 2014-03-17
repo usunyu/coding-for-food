10aef8e4de9f7d363313d1e452368c9adebd99f1
usunyu <usunyu@gmail.com>
2014-03-17 00:42:58 -0700

    ConstructBinaryTree refactor

--- LeetCode/ConstructBinaryTreefromPreorderandInorderTraversal.java
+++ LeetCode/ConstructBinaryTreefromPreorderandInorderTraversal.java
@@ -6,44 +6,9 @@ You may assume that duplicates do not exist in the tree.
 */
 
 import java.util.*;
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int x) { val = x; }
-}
+import LCLibrary.*;
 
 class Solution {
-    // from BinaryTreeLevelOrderTraversal.java for testing
-    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
-        ArrayList<ArrayList<Integer>> levels = new ArrayList<ArrayList<Integer>>();
-        if(root == null) {
-            return levels;
-        }
-        LinkedList<TreeNode> nodeQueue = new LinkedList<TreeNode>();
-        nodeQueue.add(root);
-        while(!nodeQueue.isEmpty()) {
-            LinkedList<TreeNode> tempQueue = new LinkedList<TreeNode>();
-            ArrayList<Integer> level = new ArrayList<Integer>();
-            while(!nodeQueue.isEmpty()) {
-                TreeNode node = nodeQueue.removeFirst();
-                if(node.left != null) {
-                    tempQueue.add(node.left);
-                }
-                if(node.right != null) {
-                    tempQueue.add(node.right);
-                }
-                level.add(node.val);
-            }
-            // finish one level
-            levels.add(level);
-            nodeQueue = tempQueue;
-        }
-        return levels;
-    }
-
     public TreeNode buildTree(int[] preorder, int pstart, int pend, int[] inorder, int istart, int iend) {
         if(pstart <= pend && istart <= iend) {
             TreeNode local = new TreeNode(preorder[pstart]);
@@ -135,21 +100,11 @@ class Solution {
 }
 
 class Main {
-    public static void print(ArrayList<ArrayList<Integer>> levels) {
-        for(ArrayList<Integer> level : levels) {
-            for(int val : level) {
-                System.out.print(val + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] preorder = {1,2};
         int[] inorder = {1,2};
         TreeNode root = solution.buildTree3(preorder, inorder);
-        print(solution.levelOrder(root));
+        Output.levelOrderTraversalTree(root);
     }
 }
--- LeetCode/LCLibrary/Output.java
+++ LeetCode/LCLibrary/Output.java
@@ -1,5 +1,8 @@
 package LCLibrary;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Output {
 	public static void printList(ListNode node) {
         while(node != null) {
@@ -10,5 +13,47 @@ public class Output {
             node = node.next;
         }
         System.out.println();
+    }
+
+    public static void printLevelLists(ArrayList<ArrayList<Integer>> levels) {
+        for(ArrayList<Integer> level : levels) {
+        	System.out.print("[");
+        	for(int i = 0; i < level.size(); i++) {
+        		if(i == level.size() - 1)
+        			System.out.print(level.get(i));
+        		else
+        			System.out.print(level.get(i) + ", ");
+        	}
+            System.out.println("]");
+        }
+    }
+
+    // from BinaryTreeLevelOrderTraversal.java for testing
+    public static void levelOrderTraversalTree(TreeNode root) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        ArrayList<ArrayList<Integer>> levels = new ArrayList<ArrayList<Integer>>();
+        if(root == null) {
+            return;
+        }
+        LinkedList<TreeNode> nodeQueue = new LinkedList<TreeNode>();
+        nodeQueue.add(root);
+        while(!nodeQueue.isEmpty()) {
+            LinkedList<TreeNode> tempQueue = new LinkedList<TreeNode>();
+            ArrayList<Integer> level = new ArrayList<Integer>();
+            while(!nodeQueue.isEmpty()) {
+                TreeNode node = nodeQueue.removeFirst();
+                if(node.left != null) {
+                    tempQueue.add(node.left);
+                }
+                if(node.right != null) {
+                    tempQueue.add(node.right);
+                }
+                level.add(node.val);
+            }
+            // finish one level
+            levels.add(level);
+            nodeQueue = tempQueue;
+        }
+        printLevelLists(levels);
     }
 }

b155db81fbbcc340caf507660d2c5c10608eaff5
usunyu <usunyu@gmail.com>
2014-03-18 00:31:08 -0700

    CTCI Q1.8

--- Cracking Coding Interview/Chap1 - Arrays and Strings/Q1.8.java
+++ Cracking Coding Interview/Chap1 - Arrays and Strings/Q1.8.java
@@ -1,7 +1,13 @@
+/*
+Assume you have a method isSubstring which checks if one word is a substring of another.
+Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call
+to isSubstring (e.g.,"waterbottle" is a rotation of "erbottlewat").
+*/
+
 import java.util.*;
 
-class Q1_8App {
-
+class Solution {
+	/*
 	public static boolean isSubstring(String s1, String s2) {
 		if(s1.length() < s2.length())
 			return false;
@@ -39,5 +45,58 @@ class Q1_8App {
 			System.out.println("Is Rotation.");
 		else
 			System.out.println("Not rotation.");
+	}
+	*/
+	/*
+		Second Round
+	*/
+	public static boolean isSubstring(String s1, String s2) {
+		if(s2.length() > s1.length()) return false;
+		// preprocess
+		int[] ls = new int[s2.length()];
+		int i = 1, length = 0;
+		while(i < s2.length()) {
+			if(s2.charAt(i) == s2.charAt(length)) {
+				ls[i++] = ++length;
+			}
+			else if(length > 0) {
+				length = ls[length - 1];
+			}
+			else i++;
+		}
+		// kmp
+		i = 0; int j = 0;
+		while(i < s1.length()) {
+			if(s1.charAt(i) == s2.charAt(j)) {
+				i++; j++;
+			}
+			else if(j > 0) {
+				j = ls[j - 1];
+			}
+			else i++;
+			if(j == s2.length()) return true;
+		}
+		return false;
+	}
+
+	public static boolean isRotation(String s1, String s2) {
+		int len = s1.length();
+	    /* check that s1 and s2 are equal length and not empty */
+	    if (len == s2.length() && len > 0) { 
+	    	/* concatenate s1 and s1 within new buffer */
+	    	String s1s1 = s1 + s1;
+	    	return isSubstring(s1s1, s2);
+	    }
+	    return false;
+	}
+
+	public static void main(String[] args) {
+		String[][] pairs = {{"apple", "pleap"}, {"waterbottle", "erbottlewat"}, {"camera", "macera"}};
+		for (String[] pair : pairs) {
+			String word1 = pair[0];
+			String word2 = pair[1];
+			boolean is_rotation = isRotation(word1, word2);
+			System.out.println(word1 + ", " + word2 + ": " + is_rotation);
+		}
 	}
 }

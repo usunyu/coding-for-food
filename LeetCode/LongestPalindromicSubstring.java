868a820253e25c85fcfdd1d439b75bdaacd80150
Yu Sun <Yu.Sun@citygridmedia.com>
2014-03-19 14:16:14 -0700

    Longest Palindromic Substring

--- LeetCode/LongestPalindromicSubstring.java
+++ LeetCode/LongestPalindromicSubstring.java
@@ -1,3 +1,8 @@
+/*
+Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000, 
+and there exists one unique longest palindromic substring.
+*/
+
 class Solution {
 
     // Time Limit Exceeded

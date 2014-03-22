b95a29c7fc8ebb9f959e5ca315c519038dafc63b
Yu Sun <Yu.Sun@citygridmedia.com>
2014-03-21 17:41:20 -0700

    Median of Two Sorted Arrays

--- LeetCode/MedianofTwoSortedArrays.java
+++ LeetCode/MedianofTwoSortedArrays.java
@@ -1,4 +1,6 @@
-
+/*
+There are two sorted arrays A and B of size m and n respectively. Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+*/
 class Solution {
     public int findKth(int[] A, int aStart, int aLength, int[] B, int bStart, int bLength, int k) {
         // always assume that A is equal or smaller than B
@@ -38,6 +40,26 @@ class Solution {
             return (findKth(A, 0, m, B, 0, n, (m + n) / 2) + findKth(A, 0, m, B, 0, n, (m + n) / 2 + 1)) / 2.0;
         }
     }
+    /*
+        Second Round
+    */
+    private double findKthSortedArrays(int A[], int aStart, int aLength, int B[], int bStart, int bLength, int K) {
+        if(aLength > bLength) return findKthSortedArrays(B, bStart, bLength, A, aStart, aLength, K);
+        if(aLength == 0) return B[K - 1];
+        if(K == 1) return Math.min(A[aStart], B[bStart]);
+        int aMid = Math.min(K / 2, aLength), bMid = K - aMid;
+        int aPointer = aStart + aMid, bPointer = bStart + bMid;
+        if(A[aPointer - 1] < B[bPointer - 1]) return findKthSortedArrays(A, aPointer, aLength - aMid, B, bStart, bLength, K - aMid);
+        else if(A[aPointer - 1] > B[bPointer - 1]) return findKthSortedArrays(A, aStart, aLength, B, bPointer, bLength - bMid, K - bMid);
+        else return A[aPointer - 1];
+    }
+    
+    public double findMedianSortedArrays2(int A[], int B[]) {
+        int m = A.length, n = B.length;
+        if((m + n) % 2 == 1) return findKthSortedArrays(A, 0, A.length, B, 0, B.length, (m + n) / 2 + 1);
+        else return (findKthSortedArrays(A, 0, m, B, 0, n, (m + n) / 2 + 1) + 
+                    findKthSortedArrays(A, 0, m, B, 0, n, (m + n) / 2)) / 2.0;
+    }
 }
 
 class Main {
@@ -45,6 +67,6 @@ class Main {
         Solution solution = new Solution();
         int A[] = {1, 2, 3};
         int B[] = {2, 3, 4, 5, 6, 7, 8};
-        System.out.println(solution.findMedianSortedArrays(A, B));
+        System.out.println(solution.findMedianSortedArrays2(A, B));
     }
 }

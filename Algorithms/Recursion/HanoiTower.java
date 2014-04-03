5f3d80c407f1726a78688e76e301edcf2c60cee0
Yu Sun <Yu.Sun@citygridmedia.com>
2014-04-03 13:20:50 -0700

    Hanoi Tower

--- Algorithms/Recursion/HanoiTower.java
+++ Algorithms/Recursion/HanoiTower.java
@@ -1,15 +1,28 @@
+/*
+http://www.geeksforgeeks.org/recursive-functions/
+*/
+
 import java.io.*;
 
 class TowersApp {
 	static int nDisks = 3;
 
+	// Assuming n-th disk is bottom disk (count down)
 	public static void doTowers(int topN, char from, char inter, char to) {
-		if(topN == 1) {
+		if(topN == 1) {	// Base case (termination condition)
 			System.out.println("Disk 1 from " + from + " to "+ to);
 		}
 		else {
+			// Move first n-1 disks from source pole
+   			// to auxiliary pole using destination as
+   			// temporary pole
 			doTowers(topN - 1, from, to, inter);
+			// Move the remaining disk from source
+   			// pole to destination pole
 			System.out.println("Disk " + topN + " from " + from + " to "+ to);
+			// Move the n-1 disks from auxiliary (now source)
+   			// pole to destination pole using source pole as
+   			// temporary (auxiliary) pole
 			doTowers(topN - 1, inter, from, to);
 		}
 	}

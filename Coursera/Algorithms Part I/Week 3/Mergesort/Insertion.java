bbf61a2b052c708b15beaddbe000c08e21ed6761
Yu Sun <Yu.Sun@citygridmedia.com>
2014-04-06 20:37:23 -0700

    Merge recursion

---
+++ Coursera/Algorithms Part I/Week 3/Mergesort/Insertion.java
@@ -0,0 +1,28 @@
+public class Insertion {
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; i++)
+            for (int j = i; j > 0; j--)
+                if (less(a[j], a[j-1]))
+                    exch(a, j, j-1);
+                else break;
+    }
+
+    public static void sort(Comparable[] a, int i, int j) {
+        for (; i <= j; i++)
+            for (int k = i; k > 0; k--)
+                if (less(a[k], a[k-1]))
+                    exch(a, k, k-1);
+                else break;
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+}

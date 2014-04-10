854aa059904d9b17c0b6c79f70adb9825d50f136
Yu Sun <Yu.Sun@citygridmedia.com>
2014-04-09 22:21:01 -0700

    Remove Duplicates from Sorted List

--- LeetCode/LCLibrary/Input.java
+++ LeetCode/LCLibrary/Input.java
@@ -158,6 +158,18 @@ public class Input {
         return node1;
 	}
 
+	public static ListNode buildExampleListWithDup() {
+		ListNode head = new ListNode(1);
+        ListNode node1 = new ListNode(2); head.next = node1;
+        ListNode node2 = new ListNode(2); node1.next = node2;
+        ListNode node3 = new ListNode(3); node2.next = node3;
+        ListNode node4 = new ListNode(3); node3.next = node4;
+        ListNode node5 = new ListNode(4); node4.next = node5;
+        ListNode node6 = new ListNode(5); node5.next = node6;
+        ListNode node7 = new ListNode(5); node6.next = node7;
+        return head;
+	}
+
 	/*
 		1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9 -> 10
 		1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9 -> 10

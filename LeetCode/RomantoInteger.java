f57d538281cfc48ce0741c9ab3e9b5485d453700
Yu Sun <Yu.Sun@citygridmedia.com>
2014-04-10 22:17:18 -0700

    Roman to Integer

--- LeetCode/RomantoInteger.java
+++ LeetCode/RomantoInteger.java
@@ -1,3 +1,9 @@
+/*
+Given a roman numeral, convert it to an integer.
+
+Input is guaranteed to be within the range from 1 to 3999.
+*/
+
 class Solution {
     public int getNumBase(char roman) {
         int numBase = -1;
@@ -28,7 +34,7 @@ class Solution {
     }
     
     public boolean compareRoman(char r1, char r2) {
-        if(r1 == '-' || r1 == '-') {
+        if(r1 == '-' || r2 == '-') {
             return false;
         }
         int n1 = getNumBase(r1);
@@ -59,9 +65,67 @@ class Solution {
     }
 }
 
+/*
+    Second Round
+*/
+class Solution2 {
+    private int getNumBase(char roman) {
+        int numBase = -1;
+        switch(roman) {
+            case 'I':
+                numBase = 1;
+                break;
+            case 'V':
+                numBase = 5;
+                break;
+            case 'X':
+                numBase = 10;
+                break;
+            case 'L':
+                numBase = 50;
+                break;
+            case 'C':
+                numBase = 100;
+                break;
+            case 'D':
+                numBase = 500;
+                break;
+            case 'M':
+                numBase = 1000;
+                break;
+        }
+        return numBase;
+    }
+    
+    private boolean less(char r1, char r2) {
+        if(r1 == '-' || r2 == '-') {
+            return false;
+        }
+        int n1 = getNumBase(r1);
+        int n2 = getNumBase(r2);
+        return n1 < n2;
+    }
+    
+    public int romanToInt(String s) {
+        int number = 0;
+        for(int i = 0; i < s.length(); i++) {
+            char roman = s.charAt(i);
+            char nextRoman = (i + 1 < s.length() ? s.charAt(i + 1) : '-');
+            if(less(roman, nextRoman)) {
+                number += (getNumBase(nextRoman) - getNumBase(roman));
+                i++;
+            }
+            else {
+                number += getNumBase(roman);
+            }
+        }
+        return number;
+    }
+}
+
 class Main {
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        Solution2 solution = new Solution2();
         String roman = "LXXXVII";
         System.out.println(solution.romanToInt(roman));
     }

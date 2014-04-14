fa2d9726b591d64082a5f5aa82e6e39d66259602
Yu Sun <Yu.Sun@citygridmedia.com>
2014-04-13 21:28:49 -0700

    Search Insert Position

--- LeetCode/SearchInsertPosition.java
+++ LeetCode/SearchInsertPosition.java
@@ -5,10 +5,10 @@ If not, return the index where it would be if it were inserted in order.
 You may assume no duplicates in the array.
 
 Here are few examples.
-[1,3,5,6], 5 → 2
-[1,3,5,6], 2 → 1
-[1,3,5,6], 7 → 4
-[1,3,5,6], 0 → 0
+[1,3,5,6], 5 -> 2
+[1,3,5,6], 2 -> 1
+[1,3,5,6], 7 -> 4
+[1,3,5,6], 0 -> 0
 */
 
 class Solution {

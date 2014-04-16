10638c98473402dad8dba6edfca30a86d95ce0f4
Yu Sun <Yu.Sun@citygridmedia.com>
2014-04-16 10:53:35 -0700

    Sum Root to Leaf Numbers

--- LeetCode/SumRoottoLeafNumbers.java
+++ LeetCode/SumRoottoLeafNumbers.java
@@ -1,12 +1,22 @@
-import java.util.*;
+/*
+Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
 
-// Definition for binary tree
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int x) { val = x; }
-}
+An example is the root-to-leaf path 1->2->3 which represents the number 123.
+
+Find the total sum of all root-to-leaf numbers.
+
+For example,
+
+    1
+   / \
+  2   3
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+
+Return the sum = 12 + 13 = 25.
+*/
+import java.util.*;
+import LCLibrary.*;
 
 class Solution {
     public int dfs(TreeNode root, int num, int sum) {
@@ -33,14 +43,28 @@ class Solution {
     }
 }
 
+/*
+    Second Round
+*/
+class Solution2 {
+    private int sumNumbers(TreeNode root, int num, int sum) {
+        if(root == null) return sum;
+        num = num * 10 + root.val;
+        if(root.left == null && root.right == null) return num + sum;
+        sum = sumNumbers(root.left, num, sum);
+        sum = sumNumbers(root.right, num, sum);
+        return sum;
+    }
+    
+    public int sumNumbers(TreeNode root) {
+        return sumNumbers(root, 0, 0);
+    }
+}
+
 class Main {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        TreeNode root = new TreeNode(1);
-        TreeNode left = new TreeNode(2);
-        TreeNode right = new TreeNode(3);
-        root.left = left;
-        root.right = right;
+        Solution2 solution = new Solution2();
+        TreeNode root = Input.buildExampleTree2();
         System.out.println(solution.sumNumbers(root));
     }
 }

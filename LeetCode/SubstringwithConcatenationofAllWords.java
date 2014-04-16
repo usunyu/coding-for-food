6563ddc8f201f65186adb11b561615f1203723a2
Yu Sun <Yu.Sun@citygridmedia.com>
2014-04-15 20:24:02 -0700

    Substring with Concatenation of All Words improve

--- LeetCode/SubstringwithConcatenationofAllWords.java
+++ LeetCode/SubstringwithConcatenationofAllWords.java
@@ -131,37 +131,95 @@ class Solution3 {
         for(int i = 0; i < length; i++) {
             HashMap<String, Integer> need = new HashMap<String, Integer>(wordMap);
             int left = i, right = i;    // current window
-            while(right <= S.length() - length) {
+            while(right <= S.length() - length && left <= S.length() - N * length) {
                 String word = S.substring(right, right + length);
-                if(need.containsKey(word)) removeWord(need, word);
-                else if(wordMap.containsKey(word)) {
+                if(!wordMap.containsKey(word)) {
+                    left = right + length;
+                    right = left;
+                    need.putAll(wordMap);   // reset
+                }
+                else if(!need.containsKey(word)) {
                     left = moveLeft(need, word, S, left, right);
-                    right -= length;
                 }
                 else {
-                    left = right + length;
-                    need = new HashMap<String, Integer>(wordMap);   // reset
-                }
-                
-                right += length;
-                
-                if(need.isEmpty()) {
-                    result.add(left);
-                    if(right <= S.length() - length) {
-                        String next = S.substring(right, right + length);
-                        if(wordMap.containsKey(next)) { // we can take advantage of prevs
-                            left = moveLeft(need, next, S, left, right);
-                        }
-                        else {
-                            right += length;
-                            left = right;
-                            need = new HashMap<String, Integer>(wordMap);   // reset
+                    removeWord(need, word);
+                    right += length;
+                    if(need.isEmpty()) {
+                        result.add(left);
+                        if(right <= S.length() - length) {
+                            String next = S.substring(right, right + length);
+                            if(wordMap.containsKey(next)) { // we can take advantage of prevs
+                                left = moveLeft(need, next, S, left, right);
+                            }
+                            else {
+                                right += length;
+                                left = right;
+                                need.putAll(wordMap);   // reset
+                            }
                         }
                     }
                 }
             }
         }
         return result;
+    }
+}
+
+class Solution4 {
+    private void addWord(String w, HashMap<String, Integer> words) {
+        if (words.containsKey(w)) words.put(w, words.get(w)+1);
+        else words.put(w, 1);
+    }
+
+    private void removeWord(String w, HashMap<String, Integer> words) {
+        if (!words.containsKey(w)) return;
+        if (words.get(w) > 1) words.put(w, words.get(w)-1);
+        else words.remove(w);
+    }
+   
+    private int slideWindow(String S, int begin, int wordLen, HashMap<String, Integer> words) {
+        String old = S.substring(begin, begin+wordLen);
+        addWord(old, words);
+        return begin+wordLen;
+    }
+   
+    public ArrayList<Integer> findSubstring(String S, String[] L) {
+        ArrayList<Integer> indices = new ArrayList<Integer>();
+        if (L.length == 0) return indices;
+
+        int total = L.length, wordLen = L[0].length();
+   
+        // store the words and frequencies in a hash table
+        HashMap<String, Integer> expectWords = new HashMap<String, Integer>();
+        for (String w : L) addWord(w, expectWords);
+   
+        // find concatenations
+        for (int i=0; i < wordLen; ++i) {
+            // check if there are any concatenations
+            int count = 0;
+            HashMap<String, Integer> collectWords = new HashMap<String, Integer>(expectWords);
+            for (int j = i, begin = i; j <= S.length() - (total-count)*wordLen && begin <= S.length() - total*wordLen;) {
+                String sub = S.substring(j, j+wordLen);
+                if (!expectWords.containsKey(sub)) { // if not an expect word, reset
+                    begin = j + wordLen;
+                    j = begin;
+                    count = 0;
+                    collectWords.putAll(expectWords);
+                } else if (!collectWords.containsKey(sub)) { // if duplicate, forward begin by 1
+                    begin = slideWindow(S, begin, wordLen, collectWords);
+                } else {
+                    removeWord(sub, collectWords);
+                    j += wordLen;
+                    ++count;
+                    if (collectWords.isEmpty()) {
+                        indices.add(begin);
+                        begin = slideWindow(S, begin, wordLen, collectWords);
+                        --count;
+                    }
+                }
+            }
+        }
+        return indices;  
     }
 }
 

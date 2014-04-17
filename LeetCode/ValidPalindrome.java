234642c12fe1af580070bcf205d2c43bf861bdd4
usunyu <usunyu@gmail.com>
2014-04-17 15:22:50 -0700

    Valid Palindrome

--- LeetCode/ValidPalindrome.java
+++ LeetCode/ValidPalindrome.java
@@ -1,3 +1,16 @@
+/*
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+For example,
+"A man, a plan, a canal: Panama" is a palindrome.
+"race a car" is not a palindrome.
+
+Note:
+Have you consider that the string might be empty? This is a good question to ask during an interview.
+
+For the purpose of this problem, we define empty string as valid palindrome.
+*/
+
 class Solution {
     public boolean isAlphabet(char ch) {
         if(ch - 'a' >= 0 && ch - 'a' <= 25) {
@@ -59,6 +72,35 @@ class Solution {
             }
         }
         return isPalindrome;
+    }
+}
+/*
+    Second Round
+*/
+class Solution2 {
+    private boolean isAlphabet(char ch) { return (ch - 'a' >= 0 && ch - 'a' <= 25) || (ch - 'A' >= 0 && ch - 'A' <= 25); }
+    
+    private boolean isNumber(char ch) { return ch - '0' >= 0 && ch - '0' <= 9; }
+    
+    private boolean isSameChar(char ch1, char ch2) {
+        if(isAlphabet(ch1) && isAlphabet(ch2))
+            return ch1 == ch2 || Math.abs(ch1 - ch2) == 'a' - 'A';
+        if(isNumber(ch1) && isNumber(ch2))
+            return ch1 == ch2;
+        return false;
+    }
+    
+    public boolean isPalindrome(String s) {
+        if(s == null || s.length() == 0) return true;
+        int left = 0, right = s.length() - 1;
+        while(left <= right) {
+            while(left <= right && !isAlphabet(s.charAt(left)) && !isNumber(s.charAt(left))) left++;
+            while(left <= right && !isAlphabet(s.charAt(right)) && !isNumber(s.charAt(right))) right--;
+            if(left > right) return true;
+            if(!isSameChar(s.charAt(left), s.charAt(right))) return false;
+            left++; right--;
+        }
+        return true;
     }
 }
 

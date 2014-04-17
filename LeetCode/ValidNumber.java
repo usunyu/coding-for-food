21b32ce924350ee457dccba7f791e905e1ec440f
usunyu <usunyu@gmail.com>
2014-04-17 14:21:36 -0700

    Valid Number

--- LeetCode/ValidNumber.java
+++ LeetCode/ValidNumber.java
@@ -1,3 +1,16 @@
+/*
+Validate if a given string is numeric.
+
+Some examples:
+"0" => true
+" 0.1 " => true
+"abc" => false
+"1 a" => false
+"2e10" => true
+Note: It is intended for the problem statement to be ambiguous. 
+You should gather all requirements up front before implementing one.
+*/
+
 class Solution {
     public boolean isNumber(String s) {
         // Note: The Solution object is instantiated only once and is reused by each test case.
@@ -55,11 +68,69 @@ class Solution {
         return isNumber;
     }
 }
+/*
+    Second Round
+*/
+class Solution2 {
+    private boolean isDigit(char ch) { return ch - '0' <= 9 && ch - '0' >= 0; }
+    
+    private boolean isSign(char ch) { return ch == '+' || ch == '-'; }
+    
+    private boolean isE(char ch) { return ch == 'e' || ch == 'E'; }
+    
+    private boolean isPoint(char ch) { return ch == '.'; }
+    
+    private boolean prevDigit(int current, int left, String s) {
+        for(int i = current; i >= left; i--) {
+            char ch = s.charAt(i);
+            if(isDigit(ch)) return true;
+        }
+        return false;
+    }
+    
+    private boolean nextDigit(int current, int right, String s) {
+        for(int i = current; i <= right; i++) {
+            char ch = s.charAt(i);
+            if(isDigit(ch)) return true;
+        }
+        return false;
+    }
+    
+    public boolean isNumber(String s) {
+        if(s == null || s.length() == 0) return false;
+        int left = 0, right = s.length() - 1;
+        // skip space
+        while(left <= right && s.charAt(left) == ' ') left++;
+        while(left <= right && s.charAt(right) == ' ') right--;
+        if(left > right) return false;
+        boolean sign = true, e = true, point = true;
+        for(int i = left; i <= right; i++) {
+            char ch = s.charAt(i);
+            if(isDigit(ch)) {
+                sign = false;   // when we have digit, no sign follow
+            }
+            else if(isPoint(ch) && point && (prevDigit(i - 1, left, s) || nextDigit(i + 1, right, s))) {
+                point = false;  // when we have point, no point follow
+                sign = false;   // and no sign between sign and other digit
+            }
+            else if(isSign(ch) && sign) {
+                sign = false;   // when we have sign, no sign follow
+            }
+            else if(isE(ch) && e && prevDigit(i - 1, left, s) && nextDigit(i + 1, right, s)) {
+                e = false;  // when we have e, no e follow
+                point = false;  // and no sign follow
+                sign = true;    // enable sign again, e.g. 10e+6
+            }
+            else return false;
+        }
+        return true;
+    }
+}
 
 class Main {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        String number = "005047e+6";
+        Solution2 solution = new Solution2();
+        String number = "005.047e+6";
         System.out.println(solution.isNumber(number));
     }
 }

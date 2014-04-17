cd1b5b96d69a2f198f0c647ecd0995ff01b50ab6
usunyu <usunyu@gmail.com>
2014-04-17 16:30:20 -0700

    Valid Parentheses

--- LeetCode/ValidParentheses.java
+++ LeetCode/ValidParentheses.java
@@ -1,3 +1,9 @@
+/*
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+*/
+
 import java.util.*;
 
 class Solution {
@@ -63,7 +69,26 @@ class Solution {
         return isValid;
     }
 }
-
+/*
+    Second Round
+*/
+class Solution2 {
+    public boolean isValid(String s) {
+        if(s == null || s.length() == 0) return false;
+        Stack<Character> stack = new Stack<Character>();
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '[' || ch == '{') stack.push(ch);
+            else if(!stack.isEmpty()) {
+                if(ch == ')' && stack.pop() != '(') return false;
+                if(ch == ']' && stack.pop() != '[') return false;
+                if(ch == '}' && stack.pop() != '{') return false;
+            }
+            else return false;
+        }
+        return stack.isEmpty();
+    }
+}
 class Main {
 
     public static void main(String[] args) {

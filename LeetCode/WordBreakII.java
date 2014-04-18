52682a2b07be65a074931358bd3c5bb717ff207e
usunyu <usunyu@gmail.com>
2014-04-18 01:12:51 -0700

    Word Break II

--- LeetCode/WordBreakII.java
+++ LeetCode/WordBreakII.java
@@ -1,3 +1,15 @@
+/*
+Given a string s and a dictionary of words dict, add spaces in s to construct a sentence where each word is a valid dictionary word.
+
+Return all such possible sentences.
+
+For example, given
+s = "catsanddog",
+dict = ["cat", "cats", "and", "sand", "dog"].
+
+A solution is ["cats and dog", "cat sand dog"].
+*/
+
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.HashSet;
@@ -33,21 +45,60 @@ class Solution {
         return result;
     }
 }
+/*
+    Second Round
+*/
+// dynamic programming
+class Solution2 {
+    private boolean wordBreakPossibile(String s, Set<String> dict) {
+        if(s == null || s.length() == 0) return false;
+        int N = s.length();
+        boolean[] cache = new boolean[N];
+        for(int i = 0; i < N; i++) {
+            if(dict.contains(s.substring(0, i + 1))) {
+                cache[i] = true;
+                continue;
+            }
+            for(int j = i - 1; j >= 0; j--) {
+                if(cache[j] && dict.contains(s.substring(j + 1, i + 1))) {
+                    cache[i] = true;
+                    break;
+                }
+            }
+        }
+        return cache[N - 1];
+    }
+
+    public ArrayList<String> wordBreak(String s, Set<String> dict) {
+        if(s == null || s.length() == 0) return new ArrayList<String>();
+        if(!wordBreakPossibile(s, dict)) return new ArrayList<String>();
+        int N = s.length();
+        ArrayList<ArrayList<String>> cache = new ArrayList<ArrayList<String>>();
+        for(int i = 0; i < N; i++) {
+            ArrayList<String> strings = new ArrayList<String>();
+            String str = s.substring(0, i + 1);
+            if(dict.contains(str)) strings.add(str);
+            for(int j = 0; j < i; j++) {
+                str = s.substring(j + 1, i + 1);
+                if(cache.get(j).size() > 0 && dict.contains(str)) {
+                    for(String prev : cache.get(j)) {
+                        StringBuilder sb = new StringBuilder(prev);
+                        sb.append(" "); sb.append(str);
+                        strings.add(sb.toString());
+                    }
+                }
+            }
+            cache.add(strings);
+        }
+        return cache.get(N - 1);
+    }
+}
 
 class Main {
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        Solution2 solution = new Solution2();
         Set<String> dict = new HashSet<String>();
-        dict.add("a");
-        dict.add("aa");
-        dict.add("aaa");
-        dict.add("aaaa");
-        dict.add("aaaaa");
-        dict.add("aaaaaa");
-        dict.add("aaaaaaa");
-        dict.add("aaaaaaaa");
-        dict.add("aaaaaaaaa");
-        dict.add("aaaaaaaaaa");
+        dict.add("a"); dict.add("aa"); dict.add("aaa"); dict.add("aaaa"); dict.add("aaaaa"); dict.add("aaaaaa"); dict.add("aaaaaaa"); dict.add("aaaaaaaa"); dict.add("aaaaaaaaa"); dict.add("aaaaaaaaaa");
         System.out.println(solution.wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab", dict));
     }
 }

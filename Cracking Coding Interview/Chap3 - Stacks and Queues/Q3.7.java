ef8e0d3cc557160988aba639795644caeea3ef5a
usunyu <usunyu@gmail.com>
2014-04-19 15:21:56 -0700

    CTCI Chap3 Done

--- Cracking Coding Interview/Chap3 - Stacks and Queues/Q3.7.java
+++ Cracking Coding Interview/Chap3 - Stacks and Queues/Q3.7.java
@@ -1,3 +1,12 @@
+/*
+An animal shelter holds only dogs and cats, and operates on a strictly "first in, first out" basis. 
+People must adopt either the "oldest" (based on arrival time) of all animals at the shelter, or they can 
+select whether they would prefer a dog or a cat (and will receive the oldest animal of that type). 
+They cannot select which specific animal they would like. Create the data structures to maintain 
+this system and implement opera- tions such as enqueue, dequeueAny, dequeueDog and dequeueCat. 
+You may use the built-in LinkedList data structure.
+*/
+
 import java.util.*;
 
 class MyNode {
@@ -88,7 +97,7 @@ class MyQueue {
 	}
 }
 
-class Q3_7App {
+class Solution {
 	public static void cong(String pet) {
 		System.out.println();
 		System.out.println("Congratulation! You get " + pet);

928cafa7cd456114ee98a81b88601f7c55f56144
usunyu <usunyu@gmail.com>
2014-04-20 13:53:22 -0700

    CTCI Q9.6

--- Cracking Coding Interview/Chap9 - Recursion and Dynamic Programming/Q9.6.java
+++ Cracking Coding Interview/Chap9 - Recursion and Dynamic Programming/Q9.6.java
@@ -1,6 +1,10 @@
+/*
+Implement an algorithm to print all valid (i.e., properly opened and closed) combinations of n-pairs of parentheses.
+*/
+
 import java.util.*;
 
-class Q9_6App {
+class Solution {
 	public static HashSet<Integer> dupSet = new HashSet<Integer>();
 
 	public static int hashCode(ArrayList<Character> list) {
@@ -54,3 +58,34 @@ class Q9_6App {
 		printList(pList);
 	}
 }
+/*
+	Second Round
+*/
+class Solution2 {
+	private static void generateParentheses(int left, int right, StringBuilder sb, ArrayList<String> result) {
+		if(left == 0 && right == 0) result.add(sb.toString());
+		if(left > 0) {
+			sb.append('(');
+			generateParentheses(left - 1, right, sb, result);
+			sb.deleteCharAt(sb.length() - 1);	// backtracking
+		}
+		if(left < right) {
+			sb.append(')');
+			generateParentheses(left, right - 1, sb, result);
+			sb.deleteCharAt(sb.length() - 1);	// backtracking
+		}
+	}
+
+	public static ArrayList<String> generateParentheses(int n) {
+		ArrayList<String> result = new ArrayList<String>();
+		generateParentheses(n, n, new StringBuilder(), result);
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int n = 4;
+		for(String str : generateParentheses(n))
+			System.out.println(str);
+	}
+}
+

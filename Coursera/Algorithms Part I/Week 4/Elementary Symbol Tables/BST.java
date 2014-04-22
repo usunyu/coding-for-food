5965691d8cd7433fb78cfed2dd96d6ea7a886e0e
usunyu <usunyu@gmail.com>
2014-04-22 12:23:53 -0700

    Ordered Operations in BSTs

--- Coursera/Algorithms Part I/Week 4/Elementary Symbol Tables/BST.java
+++ Coursera/Algorithms Part I/Week 4/Elementary Symbol Tables/BST.java
@@ -5,6 +5,7 @@ public class BST<Key extends Comparable<Key>, Value> {
 		private Key key;
 		private Value val;
 		private Node left, right;
+		private int count;	// number of nodes in subtree
 
 		public Node(Key key, Value val) {
 			this.key = key;
@@ -25,6 +26,7 @@ public class BST<Key extends Comparable<Key>, Value> {
 			x.right = put(x.right, key, val);
 		else if (cmp == 0)
 			x.val = val;
+		x.count = 1 + size(x.left) + size(x.right);
 		return x;
 	}
 
@@ -39,9 +41,56 @@ public class BST<Key extends Comparable<Key>, Value> {
 		return null;
 	}
 
+	public Key floor(Key key) {
+		Node x = floor(root, key);
+		if (x == null) return null;
+		return x.key;
+	}
+
+	private Node floor(Node x, Key key) {
+		if (x == null) return null;
+		int cmp = key.compareTo(x.key);
+		if (cmp == 0) return x;
+		if (cmp < 0) return floor(x.left, key);
+		Node t = floor(x.right, key);
+		if (t != null) return t;
+		else return x;
+	}
+
+	public int rank(Key key) {
+		return rank(key, root);
+	}
+
+	private int rank(Key key, Node x) {
+		if (x == null) return 0;
+		int cmp = key.compareTo(x.key);
+		if (cmp < 0) return rank(key, x.left); 
+		else if (cmp > 0) return 1 + size(x.left) + rank(key, x.right); 
+		else if (cmp == 0) return size(x.left);
+	}
+
+	public int size() {
+		return size(root);
+	}
+
+	private int size(Node x) {
+		if (x == null) return 0;
+		return x.count;
+	}
+
 	public void delete(Key key)
 	{ /* see next slides */ }
 
-	public Iterable<Key> iterator()
-	{ /* see next slides */ }
+	public Iterable<Key> iterator() {
+		Queue<Key> q = new Queue<Key>();
+		inorder(root, q);
+		return q;
+	}
+
+	private void inorder(Node x, Queue<Key> q) {
+		if (x == null) return;
+		inorder(x.left, q);
+		q.enqueue(x.key);
+		inorder(x.right, q);
+	}
 }

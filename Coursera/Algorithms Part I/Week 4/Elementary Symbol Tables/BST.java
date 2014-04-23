2b800e2724973df23aab1c1c51e670243e8e530a
usunyu <usunyu@gmail.com>
2014-04-23 12:10:25 -0700

    Deletion in BSTs

--- Coursera/Algorithms Part I/Week 4/Elementary Symbol Tables/BST.java
+++ Coursera/Algorithms Part I/Week 4/Elementary Symbol Tables/BST.java
@@ -78,8 +78,37 @@ public class BST<Key extends Comparable<Key>, Value> {
 		return x.count;
 	}
 
-	public void delete(Key key)
-	{ /* see next slides */ }
+	public void deleteMin() {
+		root = deleteMin(root);
+	}
+
+	private Node deleteMin(Node x) {
+		if (x.left == null) return x.right;
+		x.left = deleteMin(x.left);
+		x.count = 1 + size(x.left) + size(x.right);
+		return x;
+	}
+
+	public void delete(Key key) {
+		root = delete(root, key);
+	}
+
+	private Node delete(Node x, Key key) {
+		if (x == null) return null;
+		int cmp = key.compareTo(x.key);
+		if      (cmp < 0) x.left  = delete(x.left,  key);
+		else if (cmp > 0) x.right = delete(x.right, key);
+		else {
+			if (x.right == null) return x.left;
+			if (x.left  == null) return x.right;
+			Node t = x;
+			x = min(t.right);
+			x.right = deleteMin(t.right);
+			x.left = t.left;
+		}
+		x.count = size(x.left) + size(x.right) + 1;
+		return x;
+	}
 
 	public Iterable<Key> iterator() {
 		Queue<Key> q = new Queue<Key>();

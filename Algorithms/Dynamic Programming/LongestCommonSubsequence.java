cbcbfaa4ab5b404468d42d67b3134268e6446c46
usunyu <usunyu@gmail.com>
2014-04-23 17:32:24 -0700

    DP Problem Statement

--- Algorithms/Dynamic Programming/LongestCommonSubsequence.java
+++ Algorithms/Dynamic Programming/LongestCommonSubsequence.java
@@ -1,4 +1,9 @@
 /*
+LCS Problem Statement: Given two sequences, find the length of longest subsequence present in both of them. 
+A subsequence is a sequence that appears in the same relative order, but not necessarily contiguous. 
+For example, “abc”, “abg”, “bdf”, “aeg”, ‘”acefg”, .. etc are subsequences of “abcdefg”. 
+So a string of length n has 2^n different possible subsequences.
+
 http://www.geeksforgeeks.org/dynamic-programming-set-4-longest-common-subsequence/
 */
 

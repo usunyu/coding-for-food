9580e63e1738fa1ae885a16673a5d628473627cb
usunyu <usunyu@gmail.com>
2014-04-24 23:05:27 -0700

    Edit Distance

--- LeetCode/EditDistance.java
+++ LeetCode/EditDistance.java
@@ -67,9 +67,11 @@ class Solution {
         }
         return minSteps;
     }
+}
 
+class Solution2 {
     // dynamic programming solution
-    public int minDistance2(String word1, String word2) {
+    public int minDistance(String word1, String word2) {
         int m = word1.length();
         int n = word2.length();
         if(m == 0 || n == 0) return m + n;
@@ -93,10 +95,12 @@ class Solution {
         }
         return space[m][n];
     }
-    /*
-        Second Round
-    */
-    public int minDistance3(String word1, String word2) {
+}
+/*
+    Second Round
+*/
+class Solution3 {
+    public int minDistance(String word1, String word2) {
         int m = word1.length(), n = word2.length();
         if(m == 0 || n == 0) return m + n;
         int[][] dp = new int[n + 1][m + 1];
@@ -120,12 +124,39 @@ class Solution {
         return dp[n][m];
     }
 }
+/*
+    Third Round
+*/
+class Solution4 {
+    public int minDistance(String word1, String word2) {
+        if(word1 == null || word2 == null) return 0;
+        int m = word1.length(), n = word2.length();
+        if(m == 0 || n == 0) return Math.max(m, n);
+        int[][] dp = new int[m + 1][n + 1];
+        // initial
+        for(int i = 0; i <= m; i++) dp[i][0] = i;
+        for(int i = 0; i <= n; i++) dp[0][i] = i;
+        // dp
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], 
+                        Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
 
 class Main {
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        Solution4 solution = new Solution4();
         String word1 = "sea", word2 = "ate";
-        System.out.println(solution.minDistance3(word1, word2));
+        System.out.println(solution.minDistance(word1, word2));
     }
 }
 
